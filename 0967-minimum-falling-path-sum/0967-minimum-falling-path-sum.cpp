a7edf5792c62ac6c84876dd656592380ca2c6be4
Riya Raj <rrajj7674@gmail.com>
2024-10-29 20:09:45 +0530

    Time: 4 ms (19.58%), Space: 14 MB (9.81%) - LeetHub

---
+++ 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.cpp
@@ -0,0 +1,73 @@
+// class Solution {
+// public:
+
+//     int recursion(int i, int j, vector<vector<int>>&matrix, vector<vector<int>>&dp)
+//     {
+//         int n = matrix.size();
+//         if(i == n-1) return dp[i][j] = matrix[i][j];
+
+//         if(dp[i][j] != -1) return dp[i][j];
+
+//         int left = INT_MAX; 
+//         int down = INT_MAX; 
+//         int right = INT_MAX; 
+
+//         if(i<n-1 && j>0) left = matrix[i][j] + recursion(i+1, j-1, matrix, dp);
+//         if(i<n-1) down = matrix[i][j] + recursion(i+1, j, matrix, dp);
+//         if(i<n-1 && j<n-1) right = matrix[i][j] + recursion(i+1, j+1, matrix, dp);
+
+//         return dp[i][j] = min(left, min(down, right));
+//     }
+
+//     int minFallingPathSum(vector<vector<int>>& matrix) 
+//     {
+//         int n = matrix.size();
+//         vector<vector<int>>dp(n, vector<int>(n, -1));
+//         int ans = INT_MAX;
+
+//         for(int j=0; j<n; j++)
+//         {
+//             ans = min(ans, recursion(0, j, matrix, dp));
+//         }
+
+//         return ans; 
+//     }
+// };
+
+//TABULATION 
+// tabulation is a top down approach. we also did it through bottom up approach previously. 
+
+
+class Solution {
+public:
+    int minFallingPathSum(vector<vector<int>>& matrix) 
+    {
+        int n = matrix.size();
+        vector<vector<int>>table(n, vector<int>(n, 0));
+
+        for(int j=0; j<n; j++) table[n-1][j] = matrix[n-1][j]; 
+
+        for(int i=n-2; i>=0; i--)
+        {
+            for(int j=0; j<n; j++)
+            {
+                int left = INT_MAX; 
+                int down = INT_MAX; 
+                int right = INT_MAX; 
+
+                if(i<n-1 && j>0) left = matrix[i][j] + table[i+1][j-1];
+                if(i<n-1) down = matrix[i][j] + table[i+1][j];
+                if(i<n-1 && j<n-1) right = matrix[i][j] + table[i+1][j+1];
+
+                table[i][j] = min(left, min(down, right)); 
+            }
+        }
+
+        int ans = INT_MAX; 
+        for(int i=0; i<n; i++)
+        {
+            if(table[0][i] < ans) ans = table[0][i];
+        } 
+        return ans; 
+    }
+};
