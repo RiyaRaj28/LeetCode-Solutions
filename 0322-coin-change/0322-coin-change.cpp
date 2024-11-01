33e1da452798f13c04c3e1c419c3dd4723fdc840
Riya Raj <rrajj7674@gmail.com>
2024-11-01 18:47:44 +0530

    Time: 33 ms (56.34%), Space: 20.6 MB (46.45%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -29,30 +29,62 @@ public:
     //     return ans; 
     // }
 
+    // int coinChange(vector<int>& coins, int target)
+    // {
+    //     int n = coins.size(); 
+    //     vector<vector<int>>dp(n, vector<int>(target+1, 0));
+
+    //     for(int i=0; i<=target; i++)
+    //     {
+    //         if(i % coins[0] == 0) dp[0][i] = i/coins[0]; 
+    //         else dp[0][i] = 1e9; 
+    //     }
+
+    //     for(int i=1; i<n; i++)
+    //     {
+    //         for(int targ = 0; targ<= target; targ++)
+    //         {
+    //             int nottake = dp[i-1][targ];
+    //             int take = 1e9; 
+    //             if(coins[i] <= targ) take = 1+ dp[i][targ - coins[i]];
+
+    //             dp[i][targ] = min(take, nottake);  
+    //         }
+    //     }
+
+    //     int ans = dp[n-1][target]; 
+    //     if(ans >= 1e9) return -1; 
+    //     else return ans; 
+    // } 
+
     int coinChange(vector<int>& coins, int target)
     {
         int n = coins.size(); 
-        vector<vector<int>>dp(n, vector<int>(target+1, 0));
+        // vector<vector<int>>dp(n, vector<int>(target+1, 0));
+        vector<int>prev(target+1, 0), curr(target+1, 0);
 
         for(int i=0; i<=target; i++)
         {
-            if(i % coins[0] == 0) dp[0][i] = i/coins[0]; 
-            else dp[0][i] = 1e9; 
+            if(i % coins[0] == 0) prev[i] = i/coins[0]; 
+            else prev[i] = 1e9; 
         }
+
 
         for(int i=1; i<n; i++)
         {
             for(int targ = 0; targ<= target; targ++)
             {
-                int nottake = dp[i-1][targ];
+                int nottake = prev[targ];
                 int take = 1e9; 
-                if(coins[i] <= targ) take = 1+ dp[i][targ - coins[i]];
+                //dont go to previous here
+                if(coins[i] <= targ) take = 1+ curr[targ - coins[i]];
 
-                dp[i][targ] = min(take, nottake);  
+                curr[targ] = min(take, nottake);  
             }
+            prev = curr; 
         }
 
-        int ans = dp[n-1][target]; 
+        int ans = prev[target]; 
         if(ans >= 1e9) return -1; 
         else return ans; 
     } 
