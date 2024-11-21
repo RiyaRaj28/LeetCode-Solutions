7fd59332f14607c568f32350c2dfa53240987961
Riya Raj <rrajj7674@gmail.com>
2024-11-22 01:07:16 +0530

    Time: 0 ms (100%), Space: 20.7 MB (75.79%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -18,19 +18,28 @@ public:
         // }
         // return 0; 
 
+        // int n = nums.size();
+        // vector<int>hash(n+1, 0); 
+        // for(int i=0; i<n; i++)
+        // {
+        //     hash[nums[i]] = 1; 
+        // }
+
+        // for(int i=0; i<=n; i++)
+        // {
+        //     if(hash[i] == 0) return i; 
+        // }
+
+        // return 0; 
+
         int n = nums.size();
-        vector<int>hash(n+1, 0); 
+        int sum = 0; 
         for(int i=0; i<n; i++)
         {
-            hash[nums[i]] = 1; 
+            sum += nums[i];
         }
+        int total_sum = n*(n+1)/2; 
 
-        for(int i=0; i<=n; i++)
-        {
-            if(hash[i] == 0) return i; 
-        }
-
-        return 0; 
-        
+        return total_sum - sum; 
     }
 };
