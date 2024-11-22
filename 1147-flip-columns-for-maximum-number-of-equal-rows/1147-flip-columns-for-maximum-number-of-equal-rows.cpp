7387a17ca7f9d2c599ec28e06634d236f7098f8f
Riya Raj <rrajj7674@gmail.com>
2024-11-22 17:52:03 +0530

    Time: 22 ms (73.19%), Space: 74.4 MB (73.51%) - LeetHub

--- 1147-flip-columns-for-maximum-number-of-equal-rows/1147-flip-columns-for-maximum-number-of-equal-rows.cpp
+++ 1147-flip-columns-for-maximum-number-of-equal-rows/1147-flip-columns-for-maximum-number-of-equal-rows.cpp
@@ -2,30 +2,68 @@ class Solution {
 public:
     int maxEqualRowsAfterFlips(vector<vector<int>>& matrix) 
     {
-        int n = matrix.size(); //rows
-        int m = matrix[0].size(); //cols
+        // int n = matrix.size(); //rows
+        // int m = matrix[0].size(); //cols
 
-          int maxcount = 0;   
-          for(auto &vec: matrix)  //O(n)
-          {
-            vector<int>inverted(m, 0);  //O(m)
-            for(int i=0; i<m; i++)
+        //   int maxcount = 0;   
+        //   for(auto &vec: matrix)  //O(n)
+        //   {
+        //     vector<int>inverted(m, 0);  //O(m)
+        //     for(int i=0; i<m; i++)
+        //     {
+        //         inverted[i] = vec[i] == 0 ? 1 : 0;
+        //     }
+
+        //     int count = 0; 
+        //     for(auto &row : matrix)    //O(n)
+        //     {
+        //         if(row == vec || row == inverted)
+        //         count++;
+        //     }
+
+        //         maxcount = max(count, maxcount);
+        //   }
+
+        //   return maxcount; 
+
+        // we create an unordered map 
+        // for each row of the matrix
+        // find the nature of first = row[0]
+        // find nature of row = ""
+        // for the rest elems of the row 
+        // if their nature is same as that of first then add s in the nature of row string else d 
+        // mpp[natureofrow]++
+        // iterate through map and find the max value of second element and return that 
+
+        unordered_map<string, int>mpp; 
+        int n = matrix.size(); 
+        int m = matrix[0].size();
+
+        for(auto &vec: matrix)
+        {
+            string nature = "";
+            int firstval = vec[0];
+            for(int i=1; i<m; i++)
             {
-                inverted[i] = vec[i] == 0 ? 1 : 0;
+                if(vec[i] == firstval) 
+                {
+                    nature += 's';
+                }
+                else nature += 'd';
             }
 
-            int count = 0; 
-            for(auto &row : matrix)    //O(n)
+            mpp[nature]++;
+        }
+
+        int maxcount = 0;
+        for(auto &it: mpp)
+        {
+            if(it.second > maxcount)
             {
-                if(row == vec || row == inverted)
-                count++;
+                maxcount = it.second; 
             }
+        } 
 
-                maxcount = max(count, maxcount);
-          }
-
-          return maxcount; 
-
-        
+        return maxcount;
     }
 };
