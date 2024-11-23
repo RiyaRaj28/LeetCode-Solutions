fb9a3cbaf71bbb282a9687412f28e72e8f9bbdfb
Riya Raj <rrajj7674@gmail.com>
2024-11-23 19:49:23 +0530

    Time: 18 ms (79.12%), Space: 49.7 MB (94.41%) - LeetHub

---
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -0,0 +1,74 @@
+// class Solution {
+// public:
+
+//     bool linear_search(int num, vector<int>&nums)
+//     {
+//         for(int i=0; i<nums.size(); i++)
+//         {
+//             if(nums[i] == num) return true; 
+//         }
+//         return false; 
+//     }
+//     int longestConsecutive(vector<int>& nums) 
+//     {
+//         //we have to find the next greater element by 
+//         //linear search, need to find x+1 in the array in 
+//         // the reamining elems from the current one 
+
+//         //be confident 
+
+//         int maxi = 0; 
+
+//         for(int i=0; i<nums.size(); i++)
+//         {
+//             int el = nums[i];
+//             int cnt = 1; 
+
+//             while(linear_search(el+1, nums) == true)
+//             {
+//                 el = el+1; 
+//                 cnt += 1; 
+//             }
+
+//             maxi = max(maxi, cnt);
+//         }
+//         return maxi; 
+        
+//     }
+// };
+
+
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) 
+    {
+        //we have to find the next greater element by 
+        //linear search, need to find x+1 in the array in 
+        // the reamining elems from the current one 
+
+        //be confident 
+        // we will keep a variable last smallest 
+        // and we will check if arr[i]-1 == last smallest, then inc count 
+
+        int last_smallest = INT_MIN; 
+        int cnt = 1;
+        int maxi = 0; 
+        sort(nums.begin(), nums.end());
+
+        for(int i=0; i<nums.size(); i++)
+        {
+            if(nums[i]-1 == last_smallest)
+            {
+                last_smallest = nums[i];
+                cnt += 1; 
+            }
+            else if(nums[i] != last_smallest)
+            {
+                last_smallest = nums[i];
+                cnt = 1; 
+            }
+            maxi = max(maxi, cnt);
+        }
+        return maxi; 
+    }
+};
