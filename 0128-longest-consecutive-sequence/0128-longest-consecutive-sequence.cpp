b83fcbd7e7eef653862f632cea6cd5d91c283b22
Riya Raj <rrajj7674@gmail.com>
2024-11-23 20:08:31 +0530

    Time: 84 ms (50.21%), Space: 74.6 MB (59.81%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -38,37 +38,70 @@
 // };
 
 
+// class Solution {
+// public:
+//     int longestConsecutive(vector<int>& nums) 
+//     {
+//         //we have to find the next greater element by 
+//         //linear search, need to find x+1 in the array in 
+//         // the reamining elems from the current one 
+
+//         //be confident 
+//         // we will keep a variable last smallest 
+//         // and we will check if arr[i]-1 == last smallest, then inc count 
+
+//         int last_smallest = INT_MIN; 
+//         int cnt = 1;
+//         int maxi = 0; 
+//         sort(nums.begin(), nums.end());
+
+//         for(int i=0; i<nums.size(); i++)
+//         {
+//             if(nums[i]-1 == last_smallest)
+//             {
+//                 last_smallest = nums[i];
+//                 cnt += 1; 
+//             }
+//             else if(nums[i] != last_smallest)
+//             {
+//                 last_smallest = nums[i];
+//                 cnt = 1; 
+//             }
+//             maxi = max(maxi, cnt);
+//         }
+//         return maxi; 
+//     }
+// };
+
+
+
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) 
     {
-        //we have to find the next greater element by 
-        //linear search, need to find x+1 in the array in 
-        // the reamining elems from the current one 
+        // insert elements in a set and then perform functions in that 
 
-        //be confident 
-        // we will keep a variable last smallest 
-        // and we will check if arr[i]-1 == last smallest, then inc count 
-
-        int last_smallest = INT_MIN; 
-        int cnt = 1;
-        int maxi = 0; 
-        sort(nums.begin(), nums.end());
-
+        unordered_set<int>st; 
         for(int i=0; i<nums.size(); i++)
         {
-            if(nums[i]-1 == last_smallest)
-            {
-                last_smallest = nums[i];
-                cnt += 1; 
-            }
-            else if(nums[i] != last_smallest)
-            {
-                last_smallest = nums[i];
-                cnt = 1; 
-            }
-            maxi = max(maxi, cnt);
+            st.insert(nums[i]);
         }
-        return maxi; 
+
+        int longest = 0; 
+        for(auto it : st)
+        {
+            if(st.find(it - 1) == st.end())
+            {
+                int cnt = 1; 
+                int x = it; 
+                while(st.find(x+1) != st.end())
+                {
+                    cnt++; 
+                    x = x+1; 
+                }
+                longest = max(longest, cnt);
+            }
+        }
+        return longest; 
     }
 };
