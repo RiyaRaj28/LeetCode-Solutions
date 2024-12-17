9d87d1681fd0aac3e6642839e2c77ea1a94910d5
Riya Raj <rrajj7674@gmail.com>
2024-12-17 14:15:36 +0530

    Time: 25 ms (78.48%), Space: 28 MB (45.83%) - LeetHub

--- 2300-construct-string-with-repeat-limit/2300-construct-string-with-repeat-limit.cpp
+++ 2300-construct-string-with-repeat-limit/2300-construct-string-with-repeat-limit.cpp
@@ -1,57 +1,103 @@
 class Solution {
 public:
+    // string repeatLimitedString(string s, int repeatLimit) 
+    // {
+    //     int hash[26] = {0}; 
+
+    //     for(char c : s)
+    //     {
+    //         hash[c - 'a']++; 
+    //     }
+
+    //     //start traversing from end of array and filling 
+    //     string temp = "";
+    //     int i = 25;
+    //     while(i >= 0)
+    //     {
+    //         // while(i>=0 && hash[i] == 0) i--; 
+    //         if(hash[i] == 0)
+    //         {
+    //             i--;
+    //             continue; 
+    //         }
+
+    //         char c = i + 'a'; 
+    //         int freq = hash[i]; 
+    //         // int count = 0; 
+    //         int limit = min(freq, repeatLimit);
+
+    //         for(int j=0; j<limit; j++)
+    //         {
+    //             temp += c;
+    //             hash[i]--; 
+    //         }
+
+    //         if(hash[i]>0)
+    //         {
+    //             int j = i-1; 
+    //             while(j>=0 && hash[j] == 0) j--; 
+    //             if(j < 0) break; 
+
+    //             char c = j + 'a';
+    //             temp += c; 
+    //             hash[j]--; 
+    //         }
+    //     }
+
+    //     return temp; 
+
+    // }
+
     string repeatLimitedString(string s, int repeatLimit) 
     {
         int hash[26] = {0}; 
-        for(int i : hash)
-        {
-            cout << i; 
-        }
 
         for(char c : s)
         {
             hash[c - 'a']++; 
         }
 
-        for(int i : hash)
-        {
-            cout << i; 
-        }
-
         //start traversing from end of array and filling 
         string temp = "";
-        int i = 25;
-        while(i >= 0)
+        // int i = 25;
+       
+        priority_queue<char>pq; 
+        for(int i = 0; i<26; i++)
         {
-            // while(i>=0 && hash[i] == 0) i--; 
-            if(hash[i] == 0)
-            {
-                i--;
-                continue; 
-            }
-
-            char c = i + 'a'; 
-            int freq = hash[i]; 
-            // int count = 0; 
-            int limit = min(freq, repeatLimit);
-
-            for(int j=0; j<limit; j++)
-            {
-                temp += c;
-                hash[i]--; 
-            }
-
-            if(hash[i]>0)
-            {
-                int j = i-1; 
-                while(j>=0 && hash[j] == 0) j--; 
-                if(j < 0) break; 
-
-                char c = j + 'a';
-                temp += c; 
-                hash[j]--; 
-            }
+            if( hash[i] != 0) pq.push(i + 'a'); 
         }
+        // for(char c : pq) cout << c; 
+
+        //pop top element from pq and put in the heap 
+        while(!pq.empty())
+        {
+        char c = pq.top();
+        pq.pop(); 
+        int freq = hash[c - 'a'];
+
+        int limit = min(freq, repeatLimit);
+        for(int j=0; j<limit; j++)
+        {
+            temp += c; 
+            hash[c - 'a']--; 
+        }
+
+        //if still value of hash is greter 
+        if(hash[c - 'a'] > 0 && !pq.empty())
+        {
+            char nextchar = pq.top(); 
+            pq.pop(); 
+            temp += nextchar; 
+            hash[nextchar - 'a']--; 
+
+            if(hash[nextchar - 'a'] > 0) pq.push(nextchar);
+            pq.push(c);
+        }
+
+        
+
+        }
+
 
         return temp; 
 
