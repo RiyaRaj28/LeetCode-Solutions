5d8c7a5e44753c5c2ebc034e482d03402ef10d42
Riya Raj <rrajj7674@gmail.com>
2024-12-22 17:54:17 +0530

    Time: 0 ms (100%), Space: 16.4 MB (16.23%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -59,38 +59,150 @@
 //     }
 
 //SPACE OPTIMISED
+// class Solution {
+// public:
+//     int minPathSum(vector<vector<int>>& grid) {
+//         int m = grid.size(); 
+//         int n = grid[0].size(); 
+
+//         if(m==1 && n==1) return grid[0][0]; 
+
+//         //tabulation + sp optimisation -> bottom up approach. we will store the count at each cell and go on updating it. 
+
+//         vector<int>prev(n, 0);
+
+//         for(int i=0; i<m; i++)
+//         {
+//             vector<int> curr(n, 0); 
+//             for(int j=0; j<n; j++)
+//             {
+//                 if(i==0 && j==0) curr[j] = grid[i][j];
+//                 else
+//                 {
+//                     int up = INT_MAX;
+//                     int left = INT_MAX; 
+//                     if(i>0) up = prev[j];
+//                     if(j>0) left = curr[j-1]; 
+
+//                     curr[j] = grid[i][j] + min(up, left);
+//                 }
+//             }
+//             prev = curr; 
+//         }
+
+//         return prev[n-1];
+//     }
+    
+// };
+
+
+
+
+// class Solution {
+// public:
+
+//     int solve(vector<vector<int>>&grid, int i, int j)
+//     {
+//         int m = grid.size();
+//         int n = grid[0].size(); 
+//         //base case 
+//         // we are going from n-1,n-1 to 0,0
+//         if(i<0 || j<0) return 1e8; 
+
+//         if(i==0 && j==0) return grid[i][j]; 
+
+//         int up = solve(grid, i-1, j); 
+//         int left = solve(grid, i, j-1); 
+
+//         return grid[i][j] + min(up, left);
+//     }
+
+//     int minPathSum(vector<vector<int>>& grid) 
+//     {
+//         // recursive approach is simple just go from n,n to 0,0 with the correct base cases 
+
+//         // just keep moving left or up 
+
+//         // and take their minimum, the min of both the two along with the sum of the current gird elem
+
+//         // in tabulation however 
+//         // we will make a 2d dp array 
+//         // have to go to the first index 
+//         // so dp[0][0] = 0
+//         // and left will be 
+//         // and up will be 
+//         // we will start from 0,0 and go till n-1,n-1 here though 
+        
+//         int m = grid.size(); 
+//         int n = grid[0].size(); 
+//         return solve(grid, m-1, n-1);
+//     }
+
+
+    
 class Solution {
 public:
-    int minPathSum(vector<vector<int>>& grid) {
+
+    int solve(vector<vector<int>>&grid, int i, int j)
+    {
+        int m = grid.size();
+        int n = grid[0].size(); 
+        //base case 
+        // we are going from n-1,n-1 to 0,0
+        if(i<0 || j<0) return 1e8; 
+
+        if(i==0 && j==0) return grid[i][j]; 
+
+        int up = solve(grid, i-1, j); 
+        int left = solve(grid, i, j-1); 
+
+        return grid[i][j] + min(up, left);
+    }
+
+    int minPathSum(vector<vector<int>>& grid) 
+    {
+        // recursive approach is simple just go from n,n to 0,0 with the correct base cases 
+
+        // just keep moving left or up 
+
+        // and take their minimum, the min of both the two along with the sum of the current gird elem
+
+        // in tabulation however 
+        // we will make a 2d dp array 
+        // have to go to the first index 
+        // so dp[0][0] = 0
+        // and left will be 
+        // and up will be 
+        // we will start from 0,0 and go till n-1,n-1 here though 
+
+
+
+        
         int m = grid.size(); 
         int n = grid[0].size(); 
 
-        if(m==1 && n==1) return grid[0][0]; 
-
-        //tabulation + sp optimisation -> bottom up approach. we will store the count at each cell and go on updating it. 
-
-        vector<int>prev(n, 0);
+        vector<vector<int>>dp(m, vector<int>(n, 0)); 
 
         for(int i=0; i<m; i++)
         {
-            vector<int> curr(n, 0); 
             for(int j=0; j<n; j++)
             {
-                if(i==0 && j==0) curr[j] = grid[i][j];
+                if(i==0 && j==0) dp[i][j] = grid[i][i];
+
+                // have to check for the existence of down and right value and add that to the current val
+
                 else
                 {
-                    int up = INT_MAX;
-                    int left = INT_MAX; 
-                    if(i>0) up = prev[j];
-                    if(j>0) left = curr[j-1]; 
-
-                    curr[j] = grid[i][j] + min(up, left);
+                    int up = i>0 ? dp[i-1][j] : 1e8; 
+                    int left = j>0 ? dp[i][j-1] : 1e8;
+                    dp[i][j] = grid[i][j] + min(up, left);
                 }
             }
-            prev = curr; 
         }
 
-        return prev[n-1];
+        return dp[m-1][n-1]; 
     }
-    
+
+
+
 };
