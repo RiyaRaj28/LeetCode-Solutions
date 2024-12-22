46ef4daaf55340a807b49ed91c6aaaaca8006ac3
Riya Raj <rrajj7674@gmail.com>
2024-12-22 10:28:34 +0530

    Time: 0 ms (100%), Space: 8.6 MB (13.93%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,20 +1,73 @@
 class Solution {
 public:
+    // int climbStairs(int n) 
+    // {
+    //     //tabulation approach with space optimisation 
+    //     if(n<=2) return n; 
+
+    //     int n1 = 1; 
+    //     int n2 = 2; 
+
+    //     for(int i=3; i<=n; i++)
+    //     {
+    //         int curr = n1 + n2; 
+    //         n1 = n2; 
+    //         n2 = curr; 
+    //     }
+
+    //     return n2; 
+    // }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   int rec(int n, int ind, vector<int>&dp)
+   {
+    if(ind == n) return 1; 
+    if(ind > n) return 0; 
+
+    int s1 = rec(n, ind+1, dp); 
+    int s2 = rec(n, ind+2, dp); 
+
+    return dp[ind] = s1+s2; 
+   }
+
     int climbStairs(int n) 
     {
-        //tabulation approach with space optimisation 
-        if(n<=2) return n; 
+        if(n <=2) return n; 
+        vector<int>dp(n+1, -1); 
+        // return rec(n, 0, dp); 
 
-        int n1 = 1; 
-        int n2 = 2; 
+        // int onestep = 1;
+        // int twostep = 2;
+        
+        for(int i=0; i<3; i++) dp[i] = i;
+        
 
         for(int i=3; i<=n; i++)
         {
-            int curr = n1 + n2; 
-            n1 = n2; 
-            n2 = curr; 
+            int onestep = dp[i-1];
+            int twostep = dp[i-2];
+            dp[i] = onestep + twostep; 
         }
 
-        return n2; 
+        return dp[n];
+        
     }
 };
