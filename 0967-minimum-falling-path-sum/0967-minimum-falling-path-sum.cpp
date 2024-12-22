e407e3a8c68e6216c7a70f943758e5fa4b502503
Riya Raj <rrajj7674@gmail.com>
2024-12-22 16:36:57 +0530

    Time: 4 ms (19%), Space: 15 MB (17.72%) - LeetHub

--- 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.cpp
+++ 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.cpp
@@ -146,30 +146,152 @@
 // };
 
 
+// class Solution {
+// public:
+//     int minFallingPathSum(vector<vector<int>>& matrix) 
+//     {
+//        int n = matrix.size(); 
+//        vector<int>next(n, 0), curr(n, 0); 
+//        for(int i=0; i<n; i++) next[i] = matrix[n-1][i]; 
+ 
+//        for(int i=n-2; i>=0; i--)
+//        {
+//         for(int j=n-1; j>=0; j--)
+//         {
+//             int left = (j>0) ? next[j-1] : INT_MAX; 
+//             int down = next[j];   
+//             int right = (j<n-1) ? next[j+1] : INT_MAX; 
+
+//             curr[j] = matrix[i][j] + min(left, min(down, right));  
+//         }
+//         next = curr; 
+//        }
+
+//        int ans = INT_MAX;
+//        for(int i=0; i<n; i++) if(next[i] < ans) ans = next[i];
+
+//        return ans; 
+//     }
+// };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Solution {
+// public:
+//     int solve(int i, int j, vector<vector<int>>&matrix, vector<vector<int>>&dp)
+//     {
+//         int n = matrix.size();
+//         if(i > n-1 || j > n-1 || j<0) return 1e8; 
+//         if(dp[i][j] != -1) return dp[i][j]; 
+//         if(i == n-1) return matrix[i][j]; 
+
+//         int d = solve(i+1, j, matrix, dp); 
+//         int dgr = solve(i+1, j+1, matrix, dp); 
+//         int dgl = solve(i+1, j-1, matrix, dp);
+
+//         return dp[i][j] = matrix[i][j] + min(d, min(dgl, dgr)); 
+//     }
+
+//     int minFallingPathSum(vector<vector<int>>& matrix) 
+//     {
+//        int n = matrix.size();
+//        int mini = INT_MAX;
+//        vector<vector<int>>dp(n, vector<int>(n, -1)); 
+
+//        for(int j=0; j<n; j++)
+//        {
+//             mini = min(mini, solve(0, j, matrix, dp));
+//        }
+//        return mini; 
+//     }
+// };
+
+
+
+
 class Solution {
 public:
     int minFallingPathSum(vector<vector<int>>& matrix) 
     {
-       int n = matrix.size(); 
-       vector<int>next(n, 0), curr(n, 0); 
-       for(int i=0; i<n; i++) next[i] = matrix[n-1][i]; 
- 
-       for(int i=n-2; i>=0; i--)
-       {
-        for(int j=n-1; j>=0; j--)
+        // have to tabulate it 
+        // kaise karte tabulate 
+        // tabulation is bottom up 
+        // have to statrt from the small indexes then build it upwards 
+        // small index -> i = 0 to n 
+        // j -> 0 to n
+        // no. j and i have to be taken for the immediately next index 
+        // j can vary as 1, 0, -1 in the next row 
+        // have to maintain it in a table 
+        // the first row must be filled first then? 
+        // no we must operate from the last row first 
+
+        // we will fill up the last row of the table and start our traversal from the second last row 
+        // upto the 0th row 
+        int n = matrix.size();
+
+        vector<vector<int>>table(n, vector<int>(n, 0));
+
+        for(int j=0; j<n; j++)
         {
-            int left = (j>0) ? next[j-1] : INT_MAX; 
-            int down = next[j];   
-            int right = (j<n-1) ? next[j+1] : INT_MAX; 
-
-            curr[j] = matrix[i][j] + min(left, min(down, right));  
+            table[n-1][j] = matrix[n-1][j];
         }
-        next = curr; 
-       }
 
-       int ans = INT_MAX;
-       for(int i=0; i<n; i++) if(next[i] < ans) ans = next[i];
+        int ans = 1e8; 
 
-       return ans; 
+        for(int i=n-2; i>=0; i--)
+        {
+            for(int j=0; j<n; j++)
+            {
+                int d = i<n-1 ? table[i+1][j] : 1e8;
+                int dl = (i<n-1 && j>0) ? table[i+1][j-1] : 1e8;
+                int dr = (i<n-1 && j<n-1) ? table[i+1][j+1] : 1e8;
+ 
+                table[i][j] = matrix[i][j] + min(d, min(dl, dr)); 
+            }
+        }
+
+        // int ans = 1e8; 
+        for(int i=0; i<n; i++)
+        {
+            if(table[0][i] < ans) ans = table[0][i];
+        }
+
+        return ans;
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
