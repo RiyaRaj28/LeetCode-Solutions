eb5e124e6714ca53c430b2d099b54b620027be33
Riya Raj <rrajj7674@gmail.com>
2024-12-22 10:30:08 +0530

    Time: 0 ms (100%), Space: 7.8 MB (31.7%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -51,23 +51,26 @@ public:
     int climbStairs(int n) 
     {
         if(n <=2) return n; 
-        vector<int>dp(n+1, -1); 
+        // vector<int>dp(n+1, -1); 
         // return rec(n, 0, dp); 
 
-        // int onestep = 1;
-        // int twostep = 2;
+        int onestep = 1;
+        int twostep = 2;
         
-        for(int i=0; i<3; i++) dp[i] = i;
-        
+        // for(int i=0; i<3; i++) dp[i] = i;
 
         for(int i=3; i<=n; i++)
         {
-            int onestep = dp[i-1];
-            int twostep = dp[i-2];
-            dp[i] = onestep + twostep; 
+            // int onestep = dp[i-1];
+            // int twostep = dp[i-2];
+            // dp[i] = onestep + twostep; 
+
+            int curr = onestep + twostep; 
+            onestep = twostep; 
+            twostep = curr; 
         }
 
-        return dp[n];
+        return twostep;
         
     }
 };
