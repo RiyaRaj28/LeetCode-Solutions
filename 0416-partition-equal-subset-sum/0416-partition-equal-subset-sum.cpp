28210a042def6a932db83f64da6715815c9f52ea
Riya Raj <rrajj7674@gmail.com>
2024-12-23 13:58:26 +0530

    Time: 134 ms (60.22%), Space: 100.4 MB (26.15%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,44 +1,97 @@
 class Solution {
 public:
 
-    bool isPossible(vector<int>&arr, int k)
+    // bool isPossible(vector<int>&arr, int k)
+    // {
+    //     int n = arr.size();
+
+    //     //have to make a 2d table with target and index
+    //     //just store the base cases in the table 
+    //     vector<bool>prev(k+1, false), curr(k+1, false);
+    //     prev[0] = curr[0] = true;  // here the index 0 of prev and curr shows that we need to acheive a target of 0. If we need to achieve a target of 0, that means that it is possible, thus true 
+    //     if(arr[0] <= k) prev[arr[0]] = true; // prev[target] = 0 here because we want to 
+    //     // get a value of arr[0] in order to complete our target, thus at the arr[0], we have target value equal to the index, that is arr[0], so we got our target 
+
+    //     for(int i=1; i<n; i++)
+    //     {
+    //         for(int target = 1; target <= k; target++)
+    //         {
+    //             bool take = arr[i] <= target ? prev[target-arr[i]] : false; 
+    //             // check the previous values, if we get the remaning value of target at the index, then return that value else false 
+    //             bool nottake = prev[target];
+    //             curr[target] = take | nottake;
+    //         }
+    //         prev = curr;
+    //     }
+    //     return prev[k];
+    // }
+
+    // bool canPartition(vector<int>& nums) 
+    // {
+    //     //here the sum we need to find is ultimately just s/2
+    //     //if sum is even then only possible else it is not 
+
+    //     int sum = 0; 
+    //     int n = nums.size(); 
+    //     for(int i=0; i<n; i++)
+    //     {
+    //         sum += nums[i];
+    //     }
+
+    //     if(sum % 2 != 0) return false; 
+    //     else return isPossible(nums, sum/2); 
+    // }
+ 
+// lets try it via recursion and tabulation without sp opt 
+
+    bool solve(vector<int>&nums, int ind, int target, vector<vector<int>>&dp)
     {
-        int n = arr.size();
+        int n = nums.size(); 
+        if(ind == 0) return nums[ind] == target;
 
-        //have to make a 2d table with target and index
-        //just store the base cases in the table 
-        vector<bool>prev(k+1, false), curr(k+1, false);
-        prev[0] = curr[0] = true;  // here the index 0 of prev and curr shows that we need to acheive a target of 0. If we need to achieve a target of 0, that means that it is possible, thus true 
-        if(arr[0] <= k) prev[arr[0]] = true; // prev[target] = 0 here because we want to 
-        // get a value of arr[0] in order to complete our target, thus at the arr[0], we have target value equal to the index, that is arr[0], so we got our target 
+        if(dp[ind][target] != -1) return dp[ind][target];
 
-        for(int i=1; i<n; i++)
-        {
-            for(int target = 1; target <= k; target++)
-            {
-                bool take = arr[i] <= target ? prev[target-arr[i]] : false; 
-                // check the previous values, if we get the remaning value of target at the index, then return that value else false 
-                bool nottake = prev[target];
-                curr[target] = take | nottake;
-            }
-            prev = curr;
-        }
-        return prev[k];
+        bool take = nums[ind] <= target ? solve(nums, ind-1, target-nums[ind], dp) : false; 
+        bool nottake = solve(nums, ind-1, target, dp); 
+
+        return dp[ind][target] = take | nottake; 
     }
 
     bool canPartition(vector<int>& nums) 
     {
-        //here the sum we need to find is ultimately just s/2
-        //if sum is even then only possible else it is not 
-
-        int sum = 0; 
         int n = nums.size(); 
-        for(int i=0; i<n; i++)
-        {
-            sum += nums[i];
-        }
+        int sum = 0; 
 
+        for(int i=0; i<n; i++) sum += nums[i]; 
         if(sum % 2 != 0) return false; 
-        else return isPossible(nums, sum/2); 
+
+        vector<vector<int>>dp(n, vector<int>(sum/2+1, -1)); 
+        return solve(nums, n-1, sum/2, dp); 
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 };
