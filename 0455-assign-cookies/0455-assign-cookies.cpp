52e8d28faeef5796b2fe2e80079a39f66543b4d3
Riya Raj <rrajj7674@gmail.com>
2024-12-24 14:30:58 +0530

    Time: 2 ms (97.47%), Space: 45 MB (6.3%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.cpp
+++ 0455-assign-cookies/0455-assign-cookies.cpp
@@ -2,25 +2,85 @@ class Solution {
 public:
     int findContentChildren(vector<int>& g, vector<int>& s) {
         
+        // sort(g.begin(), g.end());
+        // sort(s.begin(), s.end());
+
+        // //for every greed, we have to check in the size array(in increasing order) if any cookie size is    greater than the greed 
+
+        // int i=0, j=0; 
+
+        // while(j != s.size() && i< g.size())
+        // {
+        //     int greed = g[i];
+
+        //     if(greed <= s[j])
+        //     {
+        //         i++; 
+        //     }
+                
+        //     j++; 
+        // }
+
+        // return i; 
+
+
+
+
         sort(g.begin(), g.end());
         sort(s.begin(), s.end());
 
-        //for every greed, we have to check in the size array(in increasing order) if any cookie size is    greater than the greed 
-
         int i=0, j=0; 
+        int n=g.size(), m = s.size();
 
-        while(j < s.size() && i< g.size())
+        while(i<n && j<m)
         {
-            int greed = g[i];
-
-            if(greed <= s[j])
+            if(g[i] <= s[j])
             {
-                i++; 
+                i++;
+                j++;
             }
-                
-            j++; 
+            else if(g[i] > s[j])
+            {
+                j++;
+            }
         }
 
-        return i; 
+        return i;
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 };
