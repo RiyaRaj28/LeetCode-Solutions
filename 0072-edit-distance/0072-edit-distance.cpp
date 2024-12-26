835145fe151b65521be4b641eaaa7c1990a15d73
Riya Raj <rrajj7674@gmail.com>
2024-12-26 11:56:48 +0530

    Time: 7 ms (55.97%), Space: 12.7 MB (78.49%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -58,44 +58,99 @@ public:
     //     return dp[n][m]; 
     // }
 
-int minDistance(string &word1, string &word2) 
+// int minDistance(string &word1, string &word2) 
+//     {
+//         int n = word1.size();
+//         int m = word2.size();
+
+//         // string 1 is parallel to row
+//         // string 2 is parallel to col
+
+//         // when 0th col -> parallel to col -> str2 at index 0 
+//         // meaning we have got the second string completely
+//         // or len of second string is 0 -> need 0 ops
+
+//         vector<int>prev(m+1, 0), curr(m+1, 0);
+//         for(int i=0; i<=m; i++) prev[i] = i; 
+
+//         // for(int i=0; i<=n; i++) dp[i][0] = i; 
+
+//         for(int i=1; i<=n; i++)
+//         {
+//             //need to update the current row also to satisfy the second base case 
+//             curr[0] = i; 
+//             for(int j=1; j<=m; j++)
+//             {
+//                 if(word1[i-1] == word2[j-1]) curr[j] = prev[j-1];
+//                 else 
+//                 {
+//                     int insert = 1 + prev[j-1];
+//                     int todelete = 1 + prev[j];
+//                     int replace = 1 + curr[j-1];
+
+//                     curr[j] = min(insert, min(todelete, replace));
+//                 }
+//             }
+//             prev = curr; 
+//         }
+
+//         return prev[m]; 
+//     }
+
+
+
+
+
+
+
+
+   int solve(string &word1, string &word2, int ind1, int ind2)
+   {
+        //base cases 
+        if(ind1<0) return ind2+1; 
+        if(ind2<0) return ind1+1;  
+
+        //match 
+        if(word1[ind1] == word2[ind2]) return solve(word1, word2, ind1-1, ind2-1);
+        
+            int insert = 1+ solve(word1, word2, ind1, ind2-1);
+            int todelete = 1+ solve(word1, word2, ind1-1, ind2);
+            int replace = 1 + solve(word1, word2, ind1-1, ind2-1); 
+            // return insert+todelete+replace;
+        
+
+        return min(insert, min(todelete, replace));
+   }
+
+    int minDistance(string &word1, string &word2) 
     {
-        int n = word1.size();
-        int m = word2.size();
+        int l1 = word1.length();
+        int l2 = word2.length(); 
+        vector<vector<int>>table(l1+1, vector<int>(l2+1, 0));
 
-        // string 1 is parallel to row
-        // string 2 is parallel to col
+        for(int i=0; i<=l1; i++) table[i][0] = i;
+        for(int j=0; j<=l2; j++) table[0][j] = j;
 
-        // when 0th col -> parallel to col -> str2 at index 0 
-        // meaning we have got the second string completely
-        // or len of second string is 0 -> need 0 ops
-
-        vector<int>prev(m+1, 0), curr(m+1, 0);
-        for(int i=0; i<=m; i++) prev[i] = i; 
-
-        // for(int i=0; i<=n; i++) dp[i][0] = i; 
-
-        for(int i=1; i<=n; i++)
+        for(int i=1; i<=l1; i++)
         {
-            //need to update the current row also to satisfy the second base case 
-            curr[0] = i; 
-            for(int j=1; j<=m; j++)
+            for(int j=1; j<=l2; j++)
             {
-                if(word1[i-1] == word2[j-1]) curr[j] = prev[j-1];
+                if(word1[i-1]==word2[j-1]) table[i][j] = table[i-1][j-1];
                 else 
                 {
-                    int insert = 1 + prev[j-1];
-                    int todelete = 1 + prev[j];
-                    int replace = 1 + curr[j-1];
-
-                    curr[j] = min(insert, min(todelete, replace));
+                    int insert = 1 + table[i][j-1];
+                    int todelete = 1 + table[i-1][j];
+                    int replace = 1 + table[i-1][j-1]; 
+                    table[i][j] = min(insert, min(todelete, replace));
                 }
             }
-            prev = curr; 
         }
 
-        return prev[m]; 
+        // return solve(word1, word2, l1-1, l2-1); 
+        return table[l1][l2]; 
     }
+
+
 
     
 
