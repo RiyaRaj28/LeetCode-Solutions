d9a1587155878665c6c32d90be631b66962e71b1
Riya Raj <rrajj7674@gmail.com>
2025-01-10 15:46:37 +0530

    Time: 177 ms (79.64%), Space: 115.5 MB (65.11%) - LeetHub

--- 0854-making-a-large-island/0854-making-a-large-island.cpp
+++ 0854-making-a-large-island/0854-making-a-large-island.cpp
@@ -1,7 +1,131 @@
+// class DisjointSet
+// {
+//     public : 
+//     vector<int> parent, rank, size; 
+//     DisjointSet(int n)
+//     {
+//         parent.resize(n);
+//         rank.resize(n, 0);
+//         size.resize(n, 1);
+//         for(int i=0; i<n; i++) parent[i] = i; 
+//     }
+
+//     int find(int x)
+//     {
+//         if(parent[x] == x) return x; 
+//         return parent[x] = find(parent[x]);
+//     }
+
+//     void Union(int x, int y)
+//     {
+//         int x_par = find(x);
+//         int y_par = find(y);
+
+//         if(x_par == y_par) return; 
+
+//         if(size[y_par] > size[x_par])
+//         {
+//             parent[x_par] = y_par; 
+//             size[y_par] += size[x_par];
+//         }
+//         else
+//         {
+//             parent[y_par] = x_par;
+//             size[x_par] += size[y_par];
+//         }
+//     }
+// };
+
+// class Solution {
+
+// public:
+//     int largestIsland(vector<vector<int>>& grid) 
+//     {
+//         int n = grid.size();
+//         DisjointSet ds(n*n);
+
+//         int frow[4] = {-1, 0, 1, 0};
+//         int fcol[4] = {0, 1, 0, -1}; 
+
+//         for(int i=0; i<n; i++)
+//         {
+//             for(int j=0; j<n; j++)
+//             {
+//                 if(grid[i][j] == 0) continue; 
+//                 for(int k=0; k<4; k++)
+//                 {
+//                     int nrow = frow[k] + i; 
+//                     int ncol = fcol[k] + j; 
+
+//                     if(nrow>=0 && nrow<n && ncol>=0 && ncol<n && grid[nrow][ncol] == 1)
+//                     {
+//                         int node = i * n + j; 
+//                         int anode = nrow * n + ncol; 
+//                         ds.Union(node, anode);
+//                     }
+//                 }
+//             }
+//         }
+
+//         //for each 0, chech 4-dirs if 1 is present 
+//         // if present, add to set ds 
+//         // find parent of each element in the set and find the total size of that component 
+//         // maintain in a max component variable 
+
+//         int msize = 1; 
+
+//         for(int i=0; i<n; i++)
+//         {
+//             for(int j=0; j<n; j++)
+//             {
+//                 int tsize = 1; 
+
+//                 if(grid[i][j] == 1) continue; 
+//                 else if(grid[i][j] == 0)
+//                 {
+//                     set<int> st;  
+//                     for(int k=0; k<4; k++)
+//                     {
+//                        int nrow = frow[k] + i; 
+//                        int ncol = fcol[k] + j; 
+
+//                        if(nrow>=0 && nrow<n && ncol>=0 && ncol<n && grid[nrow][ncol] == 1)
+//                        {
+//                             // we are finding the parent of the adjacent 1 and inserting it into the set to avoid adding 
+//                             // same set size again 
+//                             st.insert(ds.find(nrow * n + ncol));
+//                        }                       
+//                     }
+                   
+//                    for(auto it : st)
+//                    {
+//                       tsize += ds.size[it]; 
+//                    }
+//                 }
+//                 msize = max(msize, tsize);
+//             }
+//         }
+
+//         for(int i=0; i<n*n; i++)
+//         {
+//             msize = max(msize, ds.size[ds.find(i)]);
+//         }
+
+//         return msize; 
+//     }
+// };
+
+
+
+
+
+
+
+
 class DisjointSet
 {
-    public : 
-    vector<int> parent, rank, size; 
+    public:
+    vector<int>parent, rank, size; 
     DisjointSet(int n)
     {
         parent.resize(n);
@@ -12,7 +136,7 @@ class DisjointSet
 
     int find(int x)
     {
-        if(parent[x] == x) return x; 
+        if(x == parent[x]) return x; 
         return parent[x] = find(parent[x]);
     }
 
@@ -23,15 +147,20 @@ class DisjointSet
 
         if(x_par == y_par) return; 
 
-        if(size[y_par] > size[x_par])
+        if(size[x_par] > size[y_par])
+        {
+            parent[y_par] = x_par; 
+            size[x_par] += size[y_par]; 
+        }
+        else if(size[y_par] > size[x_par])
         {
             parent[x_par] = y_par; 
             size[y_par] += size[x_par];
         }
         else
         {
-            parent[y_par] = x_par;
-            size[x_par] += size[y_par];
+            parent[x_par] = y_par; 
+            size[y_par] += size[x_par];
         }
     }
 };
@@ -42,24 +171,25 @@ public:
     int largestIsland(vector<vector<int>>& grid) 
     {
         int n = grid.size();
-        DisjointSet ds(n*n);
+        DisjointSet ds(n*n); 
 
-        int frow[4] = {-1, 0, 1, 0};
-        int fcol[4] = {0, 1, 0, -1}; 
+        vector<int>frow = {-1, 0, 1, 0};
+        vector<int>fcol = {0, 1, 0, -1};
 
         for(int i=0; i<n; i++)
         {
             for(int j=0; j<n; j++)
             {
                 if(grid[i][j] == 0) continue; 
+                int node = i * n + j; 
+
                 for(int k=0; k<4; k++)
                 {
-                    int nrow = frow[k] + i; 
-                    int ncol = fcol[k] + j; 
-
+                    int nrow = i + frow[k];
+                    int ncol = j + fcol[k];
+                    
                     if(nrow>=0 && nrow<n && ncol>=0 && ncol<n && grid[nrow][ncol] == 1)
-                    {
-                        int node = i * n + j; 
+                    {   
                         int anode = nrow * n + ncol; 
                         ds.Union(node, anode);
                     }
@@ -67,49 +197,62 @@ public:
             }
         }
 
-        //for each 0, chech 4-dirs if 1 is present 
-        // if present, add to set ds 
-        // find parent of each element in the set and find the total size of that component 
-        // maintain in a max component variable 
-
-        int msize = 1; 
-
+        int msize = 1;
         for(int i=0; i<n; i++)
         {
             for(int j=0; j<n; j++)
             {
                 int tsize = 1; 
-
+                int node = i*n + j; 
                 if(grid[i][j] == 1) continue; 
-                else if(grid[i][j] == 0)
+                if(grid[i][j] == 0)
                 {
-                    set<int> st;  
+                    set<int>st; 
                     for(int k=0; k<4; k++)
                     {
-                       int nrow = frow[k] + i; 
-                       int ncol = fcol[k] + j; 
+                        int nrow = i + frow[k];
+                        int ncol = j + fcol[k];
 
-                       if(nrow>=0 && nrow<n && ncol>=0 && ncol<n && grid[nrow][ncol] == 1)
-                       {
-
-                        st.insert(ds.find(nrow * n + ncol));
-                       }                       
+                        if(nrow>=0 && nrow<n && ncol>=0 && ncol<n && grid[nrow][ncol] == 1)
+                        {
+                            int anode = nrow*n + ncol; 
+                            st.insert(ds.find(anode));
+                        }
                     }
-                   
-                   for(auto it : st)
-                   {
-                      tsize += ds.size[it]; 
-                   }
+                    
+                    for(auto it : st)
+                    {
+                        tsize += ds.size[it];
+                    }
                 }
-                msize = max(msize, tsize);
+                msize = max(msize, tsize); 
             }
         }
 
+        
         for(int i=0; i<n*n; i++)
         {
             msize = max(msize, ds.size[ds.find(i)]);
         }
-
-        return msize; 
+        return msize;
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
