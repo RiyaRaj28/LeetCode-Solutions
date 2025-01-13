25464fd16a3c86a767efb61276a94c83e996cf04
Riya Raj <rrajj7674@gmail.com>
2025-01-13 15:53:28 +0530

    Time: 52 ms (39.52%), Space: 32.6 MB (21.37%) - LeetHub

---
+++ 3455-minimum-length-of-string-after-operations/3455-minimum-length-of-string-after-operations.cpp
@@ -0,0 +1,73 @@
+class Solution {
+public:
+    int minimumLength(string s) 
+    {
+        // int n = s.length();
+        // vector<int>deleted(n, 0);
+        // int count = 0; 
+
+        // for(int i=0; i<n; i++)
+        // {
+        //     int l = -1; 
+        //     int r = -1; 
+        //     if(deleted[i] == 0)
+        //     {
+        //         //checking on left 
+        //         for(int j=i-1; j>=0; j--)
+        //         {
+        //             if(deleted[j] == 0 && s[j] == s[i])
+        //             {
+        //                 l = j; 
+        //             }
+        //         }
+
+        //         for(int j=i+1; j<n; j++)
+        //         {
+        //             if(deleted[j] == 0 && s[j] == s[i])
+        //             {
+        //                 r = j; 
+        //             }
+        //         }
+                
+        //         if(l != -1 && r != -1)
+        //         {
+        //             deleted[l] = 1; 
+        //             deleted[r] = 1;
+        //         }
+        //     }
+        // }
+
+        // for(int i=0; i<n; i++)
+        // {
+        //     if(deleted[i] == 0) count++; 
+        // }
+        
+        // // for(int i=0; i<n; i++)
+        // // {
+        // //     cout << deleted[i] << " ";
+        // // }
+
+        // return count; 
+
+                int n = s.length();
+        unordered_map<char, int>mpp; 
+        int count = 0; 
+        
+        for(int i=0; i<n; i++)
+        {
+            mpp[s[i]]++; 
+        }
+        
+        for(auto it = mpp.begin(); it!= mpp.end(); it++)
+        {
+            if(it->second % 2 == 0) count += 2;
+            else count += 1; 
+        }
+        
+        // for(auto it = mpp.begin(); it!= mpp.end(); it++)
+        // {
+        //     cout << it->first << " -> " << it->second << "\n";
+        // }
+        return count; 
+    }
+};
