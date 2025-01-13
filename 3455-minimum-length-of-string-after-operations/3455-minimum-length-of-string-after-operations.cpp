d2cb379910b008f4153be95c5bfdc5b771a8333c
Riya Raj <rrajj7674@gmail.com>
2025-01-13 15:57:28 +0530

    Time: 27 ms (58.87%), Space: 32.1 MB (58.06%) - LeetHub

--- 3455-minimum-length-of-string-after-operations/3455-minimum-length-of-string-after-operations.cpp
+++ 3455-minimum-length-of-string-after-operations/3455-minimum-length-of-string-after-operations.cpp
@@ -53,21 +53,36 @@ public:
         unordered_map<char, int>mpp; 
         int count = 0; 
         
-        for(int i=0; i<n; i++)
-        {
-            mpp[s[i]]++; 
-        }
+        // for(int i=0; i<n; i++)
+        // {
+        //     mpp[s[i]]++; 
+        // }
         
-        for(auto it = mpp.begin(); it!= mpp.end(); it++)
-        {
-            if(it->second % 2 == 0) count += 2;
-            else count += 1; 
-        }
+        // for(auto it = mpp.begin(); it!= mpp.end(); it++)
+        // {
+        //     if(it->second % 2 == 0) count += 2;
+        //     else count += 1; 
+        // }
         
         // for(auto it = mpp.begin(); it!= mpp.end(); it++)
         // {
         //     cout << it->first << " -> " << it->second << "\n";
         // }
+
+        vector<int>freq(26, 0); 
+        for(int i=0; i<n; i++)
+        {
+            freq[s[i]-'a']++;
+        }
+
+        for(int i=0; i<26; i++)
+        {
+            if(freq[i] != 0)
+            {
+                if(freq[i]%2 == 0) count += 2;
+                else count += 1;
+            }
+        }
         return count; 
     }
 };
