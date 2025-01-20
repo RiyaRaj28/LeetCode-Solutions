81d8520936da6120df9db706a0fb5137d4a9d57b
Riya Raj <rrajj7674@gmail.com>
2025-01-20 17:52:24 +0530

    Time: 97 ms (60.98%), Space: 161.6 MB (68.6%) - LeetHub

--- 2685-first-completely-painted-row-or-column/2685-first-completely-painted-row-or-column.cpp
+++ 2685-first-completely-painted-row-or-column/2685-first-completely-painted-row-or-column.cpp
@@ -2,35 +2,71 @@ class Solution {
 public:
     int firstCompleteIndex(vector<int>& arr, vector<vector<int>>& mat) 
     {
+        // int n = mat.size();
+        // int m = mat[0].size();
+        // unordered_map<int, pair<int, int>>mpp; 
+        // // int k = n*m; 
+        // // vector<int>freq(k);
+
+        // for(int i=0; i<n; i++)
+        // {
+        //     for(int j=0; j<m; j++)
+        //     {
+        //         mpp[mat[i][j]] = {i, j};
+        //     }
+        // }
+
+        // vector<int>rows(n, 0);
+        // vector<int>cols(m, 0);
+
+        // for(int i=0; i<arr.size(); i++)
+        // {
+        //     int x = mpp[arr[i]].first; 
+        //     int y = mpp[arr[i]].second;
+
+        //     rows[x]++; 
+        //     cols[y]++;
+        //     if(rows[x] >= m || cols[y] >= n) return i; 
+        // }
+        // return -1; 
+
         int n = mat.size();
         int m = mat[0].size();
-        unordered_map<int, pair<int, int>>mpp; 
+        unordered_map<int, int>pos; 
+        for(int i=0; i<arr.size(); i++) pos[arr[i]] = i; 
 
-        
+        // for(int i=0; i<arr.size(); i++)
+        // {
+        //     int x = mpp[arr[i]].first; 
+        //     int y = mpp[arr[i]].second;
 
-        // int k = n*m; 
-        // vector<int>freq(k);
+        //     rows[x]++; 
+        //     cols[y]++;
+        //     if(rows[x] >= m || cols[y] >= n) return i; 
+        // }
 
-        for(int i=0; i<n; i++)
+        int result = INT_MAX; 
+        for(int row=0; row<n; row++)
         {
-            for(int j=0; j<m; j++)
+            int lastind = INT_MIN; 
+            for(int col=0; col<m; col++)
             {
-                mpp[mat[i][j]] = {i, j};
+                int ind = pos[mat[row][col]];
+                lastind = max(lastind, ind);
             }
+            result = min(result, lastind);
         }
 
-        vector<int>rows(n, 0);
-        vector<int>cols(m, 0);
-
-        for(int i=0; i<arr.size(); i++)
+        for(int col=0; col<m; col++)
         {
-            int x = mpp[arr[i]].first; 
-            int y = mpp[arr[i]].second;
-
-            rows[x]++; 
-            cols[y]++;
-            if(rows[x] >= m || cols[y] >= n) return i; 
+            int lastind = INT_MIN; 
+            for(int row=0; row<n; row++)
+            {
+                int ind = pos[mat[row][col]];
+                lastind = max(lastind, ind);
+            }
+            result = min(result, lastind);
         }
-        return -1; 
+        return result; 
     }
 };
