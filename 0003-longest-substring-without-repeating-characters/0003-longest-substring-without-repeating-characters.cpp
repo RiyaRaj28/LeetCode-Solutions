b73063942b03b02d011f1cdfd39c57a4ec1a74b7
Riya Raj <rrajj7674@gmail.com>
2025-01-20 21:25:17 +0530

    Time: 7 ms (68.63%), Space: 14.3 MB (32.14%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -9,28 +9,43 @@ public:
         unordered_set<int>st; 
         int len = 0; 
 
-        while(r<n)
+        // while(r<n)
+        // {
+        //     if(st.find(s[r]) == st.end())
+        //     {
+        //         st.insert(s[r]);
+        //         r++; 
+        //         len = r-l; 
+        //     }
+        //     else
+        //     {
+        //         while(s[l] != s[r])
+        //         {
+        //             st.erase(s[l]);
+        //             l++;
+        //         }
+        //         st.erase(s[l]);
+        //         l++; 
+        //     }
+
+        //     maxlen = max(maxlen, len); 
+        // }
+        // return maxlen; 
+        
+        for(int r=0; r<n; r++)
         {
-            if(st.find(s[r]) == st.end())
+            if(st.find(s[r]) != st.end())
             {
-                st.insert(s[r]);
-                r++; 
-                len = r-l; 
-            }
-            else
-            {
-                while(s[l] != s[r])
+                while(l<r && st.find(s[r])!= st.end())
                 {
                     st.erase(s[l]);
-                    l++;
+                    l++; 
                 }
-                st.erase(s[l]);
-                l++; 
             }
-
-            maxlen = max(maxlen, len); 
+            
+            st.insert(s[r]);
+            maxlen = max(maxlen, r-l+1); 
         }
         return maxlen; 
-        
     }
 };
