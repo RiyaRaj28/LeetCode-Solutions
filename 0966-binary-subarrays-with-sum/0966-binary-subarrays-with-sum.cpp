099921bcc84b39268217685f50a432d14eac8d0b
Riya Raj <rrajj7674@gmail.com>
2025-01-21 18:17:19 +0530

    Time: 3 ms (57.45%), Space: 32.5 MB (94.32%) - LeetHub

--- 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.cpp
+++ 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.cpp
@@ -1,27 +1,61 @@
 class Solution {
 public:
-    int numSubarraysWithSum(vector<int>& nums, int goal) 
-    {
+
+    int subarraysLessThanEqualToK(vector<int>&nums, int goal)
+    {   
         int l = 0, r = 0, cnt = 0; 
         int n = nums.size(); 
         int sum = 0; 
 
-        int presum = 0; 
+        if(goal < 0) return 0; 
 
-        unordered_map<int, int>mpp; 
-        mpp[0] = 1;
-
-        for(int i=0; i<n; i++)
+        while(r < n)
         {
-            presum += nums[i];
-            int remove = presum - goal; 
+            sum += nums[r];
 
-            cnt += mpp[remove]; 
-            mpp[presum]++;
+            while(l<=r && sum > goal)
+            {
+                sum -= nums[l];
+                l++;
+            }
 
-
+            int curr_len = r - l + 1; 
+            cnt += curr_len;
+            r++; 
         }
-        return cnt; 
-        
+
+        return cnt;
+
+    }
+
+    int numSubarraysWithSum(vector<int>& nums, int goal) 
+    {
+        // int l = 0, r = 0, cnt = 0; 
+        // int n = nums.size(); 
+        // int sum = 0; 
+
+        // int presum = 0; 
+
+        // unordered_map<int, int>mpp; 
+        // mpp[0] = 1;
+
+        // for(int i=0; i<n; i++)
+        // {
+        //     presum += nums[i];
+        //     int remove = presum - goal; 
+
+        //     cnt += mpp[remove]; 
+        //     mpp[presum]++;
+
+        // }
+        // return cnt; 
+
+
+        int lessThanK = subarraysLessThanEqualToK(nums, goal);
+        int lessThanKMinusOne = subarraysLessThanEqualToK(nums, goal-1);
+
+        return lessThanK - lessThanKMinusOne; 
+
+     
     }
 };
