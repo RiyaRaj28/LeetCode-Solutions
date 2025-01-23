4996aa45d3b8b720dc0361caa8f22c7605e6ac7e
Riya Raj <rrajj7674@gmail.com>
2025-01-23 10:17:59 +0530

    Time: 23 ms (23.2%), Space: 40.4 MB (5.84%) - LeetHub

--- 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.cpp
+++ 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.cpp
@@ -30,31 +30,31 @@ public:
 
     int numSubarraysWithSum(vector<int>& nums, int goal) 
     {
-        // int l = 0, r = 0, cnt = 0; 
-        // int n = nums.size(); 
-        // int sum = 0; 
+        int l = 0, r = 0, cnt = 0; 
+        int n = nums.size(); 
+        int sum = 0; 
 
-        // int presum = 0; 
+        int presum = 0; 
 
-        // unordered_map<int, int>mpp; 
-        // mpp[0] = 1;
+        unordered_map<int, int>mpp; 
+        mpp[0] = 1;
 
-        // for(int i=0; i<n; i++)
-        // {
-        //     presum += nums[i];
-        //     int remove = presum - goal; 
+        for(int i=0; i<n; i++)
+        {
+            presum += nums[i];
+            int remove = presum - goal; 
 
-        //     cnt += mpp[remove]; 
-        //     mpp[presum]++;
+            cnt += mpp[remove]; 
+            mpp[presum]++;
 
-        // }
-        // return cnt; 
+        }
+        return cnt; 
 
 
-        int lessThanK = subarraysLessThanEqualToK(nums, goal);
-        int lessThanKMinusOne = subarraysLessThanEqualToK(nums, goal-1);
+        // int lessThanK = subarraysLessThanEqualToK(nums, goal);
+        // int lessThanKMinusOne = subarraysLessThanEqualToK(nums, goal-1);
 
-        return lessThanK - lessThanKMinusOne; 
+        // return lessThanK - lessThanKMinusOne; 
 
      
     }
