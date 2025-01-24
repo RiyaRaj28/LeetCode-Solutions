3f1ddd2c8c784636a54686d95001573e5105f41b
Riya Raj <rrajj7674@gmail.com>
2025-01-24 18:16:42 +0530

    Time: 1 ms (51.2%), Space: 13.9 MB (77.28%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
@@ -2,26 +2,65 @@ class Solution {
 public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) 
     {
+        // vector<int>temp; 
+        // int n1 = nums1.size(); 
+        // int n2 = nums2.size(); 
+        // sort(nums1.begin(), nums1.end()); 
+        // sort(nums2.begin(), nums2.end()); 
+
+        // for(int i=0; i<n1; i++)
+        // {
+        //     for(int j=0; j<n2; j++)
+        //     {
+        //         if(nums1[i] == nums2[j])
+        //         {
+        //             if(temp.empty() || nums2[j] != temp.back())
+        //             temp.push_back(nums1[i]);
+        //             break; 
+        //         }
+
+        //         if(nums2[j] > nums1[i]) break; 
+        //     }
+        // }
+        // return temp; 
+
+
+
+
+
+
+
+
         vector<int>temp; 
         int n1 = nums1.size(); 
         int n2 = nums2.size(); 
         sort(nums1.begin(), nums1.end()); 
         sort(nums2.begin(), nums2.end()); 
 
-        for(int i=0; i<n1; i++)
+        int i = 0, j = 0; 
+        while(i < n1 && j < n2)
         {
-            for(int j=0; j<n2; j++)
+            if(nums1[i] < nums2[j]) i++; 
+            else if(nums1[i] > nums2[j]) j++; 
+            else 
             {
-                if(nums1[i] == nums2[j])
+                if(temp.empty() || temp.back() != nums1[i])
                 {
-                    if(temp.empty() || nums2[j] != temp.back())
-                    temp.push_back(nums1[i]);
-                    break; 
+                    temp.push_back(nums1[i]); 
                 }
-
-                if(nums2[j] > nums1[i]) break; 
+                i++; 
+                j++;
             }
         }
         return temp; 
     }
+
+
+
+
+
+
+
+
+
 };
