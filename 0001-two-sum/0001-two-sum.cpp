c1eafe89239501b5395a4e1d46a89b4653c68cd1
Riya Raj <rrajj7674@gmail.com>
2025-02-04 16:17:36 +0530

    Time: 43 ms (32.19%), Space: 14.1 MB (76.55%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -2,30 +2,30 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         //m1 - two looops - n2
-        // for(int i=0; i<nums.size(); i++)
-        // {
-        //     for(int j=i+1; j<nums.size(); j++)
-        //     {
-        //         if(nums[i]+nums[j]==target)
-        //         {
-        //             return{i,j};
-        //         }
-        //     }
-        // }
-        // return {-1, -1}; 
-
-        // m2 - hashing
-        map<int, int> mpp; 
         for(int i=0; i<nums.size(); i++)
         {
-            int rem = target - nums[i]; 
-            if(mpp.find(rem) != mpp.end())
+            for(int j=i+1; j<nums.size(); j++)
             {
-                return {i, mpp[rem]};
+                if(nums[i]+nums[j]==target)
+                {
+                    return{i,j};
+                }
             }
-            mpp[nums[i]] = i; 
         }
         return {-1, -1}; 
+
+        // m2 - hashing
+        // map<int, int> mpp; 
+        // for(int i=0; i<nums.size(); i++)
+        // {
+        //     int rem = target - nums[i]; 
+        //     if(mpp.find(rem) != mpp.end())
+        //     {
+        //         return {i, mpp[rem]};
+        //     }
+        //     mpp[nums[i]] = i; 
+        // }
+        // return {-1, -1}; 
 
         //m3 - greedy app only for finding if nums exist or not
         //will not work since we are sorting the array so   indices   will not be the same
