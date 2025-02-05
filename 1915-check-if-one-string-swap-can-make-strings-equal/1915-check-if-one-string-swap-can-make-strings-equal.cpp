8ba0ec09b27302bc48c571f81fa86dd22c9479a3
Riya Raj <rrajj7674@gmail.com>
2025-02-05 18:19:50 +0530

    Time: 0 ms (100%), Space: 9.2 MB (9.35%) - LeetHub

--- 1915-check-if-one-string-swap-can-make-strings-equal/1915-check-if-one-string-swap-can-make-strings-equal.cpp
+++ 1915-check-if-one-string-swap-can-make-strings-equal/1915-check-if-one-string-swap-can-make-strings-equal.cpp
@@ -3,6 +3,7 @@ public:
     bool areAlmostEqual(string s1, string s2) 
     {
         int diff = 0; 
+        if(s1 == s2) return true; 
         int n1 = s1.length(), n2 = s2.length(); 
 
         int i1 = 0, i2 = 0; 
@@ -17,29 +18,33 @@ public:
             }
             else if(s1[i1] != s2[i2])
             {
-                 hash[i1] = 0; 
+                //  hash[i1] = 0; 
                 diff += 1; 
+                if(diff>2) return false; 
+
+                if(ind1 == -1) ind1 = i1; 
+                else ind2 = i1; 
                 i1++, i2++;
                
             }
-            if(diff>2) return false; 
+            // if(diff>2) return false; 
         }
 
-        if(diff == 0) return true; 
-        if(diff == 1) return false; 
+
+        // if(diff == 2)
+        // {
+        //     for(int i=0; i<n1; i++)
+        //     {
+        //         if(hash[i] == 0)
+        //         {
+        //             if(ind1 == -1) ind1 = i; 
+        //             else ind2 = i; 
+        //         }
+        //     }
+        // }
+
         if(diff == 2)
-        {
-            for(int i=0; i<n1; i++)
-            {
-                if(hash[i] == 0)
-                {
-                    if(ind1 == -1) ind1 = i; 
-                    else ind2 = i; 
-                }
-            }
-        }
-
-        if(s1[ind1] == s2[ind2] && s1[ind2] == s2[ind1]) return true; 
+            if(s1[ind1] == s2[ind2] && s1[ind2] == s2[ind1]) return true; 
         // swap(s1[ind1], s2[ind2]); 
         return false;
     }
