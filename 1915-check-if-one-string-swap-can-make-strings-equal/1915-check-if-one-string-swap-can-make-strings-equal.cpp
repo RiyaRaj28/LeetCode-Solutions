80027927b5e5fb778e20ad4abfe4ac715cf6312c
Riya Raj <rrajj7674@gmail.com>
2025-02-05 18:21:23 +0530

    Time: 0 ms (100%), Space: 9 MB (9.35%) - LeetHub

--- 1915-check-if-one-string-swap-can-make-strings-equal/1915-check-if-one-string-swap-can-make-strings-equal.cpp
+++ 1915-check-if-one-string-swap-can-make-strings-equal/1915-check-if-one-string-swap-can-make-strings-equal.cpp
@@ -12,40 +12,20 @@ public:
 
         while(i1<n1 && i2<n2)
         {
-            if(s1[i1] == s2[i2])
+            if(s1[i1] != s2[i2])
             {
-                i1++, i2++; 
-            }
-            else if(s1[i1] != s2[i2])
-            {
-                //  hash[i1] = 0; 
                 diff += 1; 
                 if(diff>2) return false; 
 
                 if(ind1 == -1) ind1 = i1; 
                 else ind2 = i1; 
-                i1++, i2++;
-               
             }
-            // if(diff>2) return false; 
+            i1++, i2++;
         }
-
-
-        // if(diff == 2)
-        // {
-        //     for(int i=0; i<n1; i++)
-        //     {
-        //         if(hash[i] == 0)
-        //         {
-        //             if(ind1 == -1) ind1 = i; 
-        //             else ind2 = i; 
-        //         }
-        //     }
-        // }
 
         if(diff == 2)
             if(s1[ind1] == s2[ind2] && s1[ind2] == s2[ind1]) return true; 
-        // swap(s1[ind1], s2[ind2]); 
+
         return false;
     }
 };
