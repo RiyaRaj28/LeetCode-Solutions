3e180c422f0445baa1be818e790dc9dff77ecd15
Riya Raj <rrajj7674@gmail.com>
2025-02-06 02:12:08 +0530

    Time: 14 ms (69.48%), Space: 147.9 MB (66.37%) - LeetHub

---
+++ 3741-reschedule-meetings-for-maximum-free-time-ii/3741-reschedule-meetings-for-maximum-free-time-ii.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int maxFreeTime(int eventTime, vector<int>& startTime, vector<int>& endTime)
+    {
+        int n = startTime.size(); 
+
+        vector<int>gap(1, startTime[0]); 
+        for(int i=1; i<n; i++) gap.push_back(startTime[i]-endTime[i-1]); 
+        gap.push_back(eventTime - endTime[n-1]); 
+
+        vector<int>largestRight(gap.size(), 0); 
+        for(int i=gap.size()-2; i>=0; i--) largestRight[i] = max(largestRight[i+1], gap[i+1]);
+        int largestLeft = 0; 
+        
+        int ans = 0;  
+        for(int i=1; i<gap.size(); i++)
+        {
+            int currint = endTime[i-1] - startTime[i-1]; 
+            if(currint <= max(largestLeft, largestRight[i])) ans = max(ans, gap[i-1] + gap[i] + currint); 
+            ans = max(ans, gap[i]+gap[i-1]); 
+            largestLeft = max(largestLeft, gap[i-1]); 
+        }
+
+        return ans; 
+
+
+
+        
+    }
+};        
