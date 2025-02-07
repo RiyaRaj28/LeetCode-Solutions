b1658805c47c486f8a8860a8a2ace3767166681b
Riya Raj <rrajj7674@gmail.com>
2025-02-07 20:05:59 +0530

    Time: 53 ms (40.96%), Space: 113.1 MB (61.72%) - LeetHub

--- 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.cpp
+++ 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.cpp
@@ -1,39 +1,70 @@
 class RandomizedSet {
 public:
-    unordered_set<int>st; 
-    RandomizedSet() {
+    // unordered_set<int>st; 
+    vector<int>vec;
+    int index = 0;  
+    unordered_map<int, int>mpp; 
 
+    // map : val -> ind
+
+    RandomizedSet() {
     }
     
     bool insert(int val) {
 
         //adds in set if not present and returns true else false 
-        if(st.find(val) == st.end()) 
+        // if(st.find(val) == st.end()) 
+        // {
+        //     st.insert(val); 
+        //     return true; 
+        // }
+
+        // return false; 
+
+        if(mpp.find(val) == mpp.end())
         {
-            st.insert(val); 
+            vec.push_back(val); 
+            mpp[val] = vec.size()-1; 
+
             return true; 
         }
 
-        return false; 
-        
+        return false;
     }
     
     bool remove(int val) {
 
         // removes and trye else f 
-        if(st.find(val) != st.end()) 
+        // if(st.find(val) != st.end()) 
+        // {
+        //     st.erase(val); 
+        //     return true; 
+        // }
+        // return false; 
+
+        if(mpp.find(val) != mpp.end())
         {
-            st.erase(val); 
+            // int temp = mpp[val]; 
+            // mpp[val] = mpp[vec.back()]; 
+            // mpp[vec.back()] = temp; 
+
+            auto it = mpp.find(val); 
+            vec[it->second] = vec.back(); 
+            vec.pop_back(); 
+            mpp[vec[it->second]] = it->second;
+            mpp.erase(val);  
+ 
             return true; 
         }
+        
         return false; 
     }
     
     int getRandom() {
         // returns a random element 
-        vector<int>vec(st.begin(), st.end()); 
+        // vector<int>vec(st.begin(), st.end()); 
 
-        int randind = rand() % st.size();
+        int randind = rand() % vec.size();
 
         return vec[randind]; 
     }
