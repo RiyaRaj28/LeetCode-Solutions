55ec0104f8b517b671f4b54bc6f42028272bcd0d
Riya Raj <rrajj7674@gmail.com>
2025-02-11 19:48:49 +0530

    Time: 0 ms (100%), Space: 14.1 MB (95.99%) - LeetHub

--- 1184-car-pooling/1184-car-pooling.cpp
+++ 1184-car-pooling/1184-car-pooling.cpp
@@ -1,32 +1,82 @@
 class Solution {
 public:
+
+    static const bool comp(vector<int>&trips1, vector<int>&trips2)
+    {
+        return trips1[1] < trips2[1]; 
+    }
+
     bool carPooling(vector<vector<int>>& trips, int capacity) 
     {
-        map<int, int>mpp; 
-        int sum = 0; 
+        //  EASY SOLUTION
+        // map<int, int>mpp; 
+        // int sum = 0; 
+
+        // for(int i=0; i<trips.size(); i++)
+        // {
+        //     int person = trips[i][0];
+        //     int pick = trips[i][1]; 
+        //     int drop = trips[i][2]; 
+
+        //     if(mpp.find(pick) == mpp.end()) mpp[pick] = person; 
+        //     else mpp[pick] += person; 
+
+        //     if(mpp.find(drop) == mpp.end()) mpp[drop] = -person; 
+        //     else mpp[drop] -= person; 
+        // }
+        // for(auto it : mpp)
+        // {
+        //     sum += (it.second); 
+
+        //     if(sum > capacity) return false;
+        // }
+        // return true; 
+
+        // STANDARD SOLUTION
+
+        sort(trips.begin(), trips.end(), comp); 
+
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>>pq; 
+
+        int total_pass = 0;
 
         for(int i=0; i<trips.size(); i++)
         {
-            int person = trips[i][0];
-            int pick = trips[i][1]; 
-            int drop = trips[i][2]; 
+            int currpass = trips[i][0]; 
+            int st = trips[i][1]; 
+            int end = trips[i][2]; 
 
-            if(mpp.find(pick) == mpp.end()) mpp[pick] = person; 
-            else mpp[pick] += person; 
+            while(!pq.empty() && st >= pq.top().first)
+            {
+                total_pass -= pq.top().second; 
+                pq.pop();
+            }
 
-            if(mpp.find(drop) == mpp.end()) mpp[drop] = -person; 
-            else mpp[drop] -= person; 
-        }
+            total_pass += currpass; 
+            if(total_pass > capacity) return false; 
 
-        for(auto it : mpp)
-        {
-            sum += (it.second); 
-
-            if(sum > capacity) return false;
-        }
+            pq.push({end, currpass}); 
+        } 
 
         return true; 
 
-        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 };
