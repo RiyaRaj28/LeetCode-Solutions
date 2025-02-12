047c35bcb8abef63d7b4655277fd4fa361a8dd24
Riya Raj <rrajj7674@gmail.com>
2025-02-12 23:32:15 +0530

    Time: 8 ms (92.65%), Space: 63.8 MB (87.26%) - LeetHub

--- 2473-max-sum-of-a-pair-with-equal-sum-of-digits/2473-max-sum-of-a-pair-with-equal-sum-of-digits.cpp
+++ 2473-max-sum-of-a-pair-with-equal-sum-of-digits/2473-max-sum-of-a-pair-with-equal-sum-of-digits.cpp
@@ -15,33 +15,77 @@ public:
 
     int maximumSum(vector<int>& nums) 
     {
-        unordered_map<long, priority_queue<int>>mpp; 
+    //     unordered_map<long, priority_queue<int>>mpp; 
 
-        for(int i=0; i<nums.size(); i++)
-        {
-            long sum = sumOfDig(nums[i]); 
-            // mpp[sum] += 1; 
+    //     for(int i=0; i<nums.size(); i++)
+    //     {
+    //         long sum = sumOfDig(nums[i]); 
+    //         // mpp[sum] += 1; 
 
-            mpp[sum].push(nums[i]); 
-        }
+    //         mpp[sum].push(nums[i]); 
+    //     }
 
-    long ans = 0; 
+    // long ans = 0; 
 
-    for(auto it=mpp.begin(); it!= mpp.end(); it++)
-    {
-        if((it->second).size() > 1)
-        {
-            long first = (it->second).top(); 
-            (it->second).pop(); 
+    // for(auto it=mpp.begin(); it!= mpp.end(); it++)
+    // {
+    //     if((it->second).size() > 1)
+    //     {
+    //         long first = (it->second).top(); 
+    //         (it->second).pop(); 
 
-            long second = (it->second).top(); 
-            (it->second).pop(); 
+    //         long second = (it->second).top(); 
+    //         (it->second).pop(); 
 
-            ans = max(ans, first+second); 
-        }
-    }
+    //         ans = max(ans, first+second); 
+    //     }
+    // }
 
-    return ans == 0 ? -1 : ans; 
+    // return ans == 0 ? -1 : ans; 
+
+   vector<int>hash(82, 0); 
+
+   // if the new element is greater than the previous stored element of
+   // the same digit sum then we update that value 
+
+   // else we will just update the maxsum every time 
+   
+   int ans = 0; 
+   for(int i=0; i<nums.size(); i++)
+   {
+      long sum = sumOfDig(nums[i]); 
+
+      if(hash[sum] == 0) hash[sum] = nums[i]; 
+      else 
+      { 
+        ans = max(ans, hash[sum]+nums[i]);
+        hash[sum] = max(hash[sum], nums[i]); 
+      }
+   }
+
+   return ans == 0 ? -1 : ans; 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 
 
