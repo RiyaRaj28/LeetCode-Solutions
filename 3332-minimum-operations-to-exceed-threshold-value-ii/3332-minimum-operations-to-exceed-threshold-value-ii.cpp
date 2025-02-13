d8ae977857be7e548d8bb25d44e6d7a593119171
Riya Raj <rrajj7674@gmail.com>
2025-02-13 07:42:57 +0530

    Time: 100 ms (38.73%), Space: 99.2 MB (16.98%) - LeetHub

--- 3332-minimum-operations-to-exceed-threshold-value-ii/3332-minimum-operations-to-exceed-threshold-value-ii.cpp
+++ 3332-minimum-operations-to-exceed-threshold-value-ii/3332-minimum-operations-to-exceed-threshold-value-ii.cpp
@@ -7,7 +7,7 @@ public:
         int steps = 0; 
         for(int i=0; i<nums.size(); i++) pq.push(nums[i]); 
 
-        while(pq.top() < k && pq.size() >= 2)
+        while(pq.top() < k)
         {
             ll f = pq.top(); 
             pq.pop(); 
@@ -15,12 +15,14 @@ public:
             ll s = pq.top();
             pq.pop(); 
 
-            pq.push(min(f,s)*2 + max(f,s)); 
+            // pq.push(min(f,s)*2 + max(f,s)); 
+            pq.push(f*2 + s);
             steps += 1; 
         }
 
-        if(pq.top() >= k) return steps; 
-        else return -1;; 
+        return steps; 
+        // if(pq.top() >= k) return steps; 
+        // else return -1;; 
 
         
     }
