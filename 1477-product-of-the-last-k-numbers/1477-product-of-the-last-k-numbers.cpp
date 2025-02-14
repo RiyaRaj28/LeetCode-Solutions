4c90faba524a960e05b2812eb5d481c4d6db3f80
Riya Raj <rrajj7674@gmail.com>
2025-02-14 20:55:17 +0530

    Time: 20 ms (68.21%), Space: 78.5 MB (18.6%) - LeetHub

--- 1477-product-of-the-last-k-numbers/1477-product-of-the-last-k-numbers.cpp
+++ 1477-product-of-the-last-k-numbers/1477-product-of-the-last-k-numbers.cpp
@@ -2,24 +2,39 @@ typedef long long ll;
 class ProductOfNumbers {
 public:
     vector<ll>vec; 
+    int size = 0; 
     ProductOfNumbers() {
-        
+        vec.push_back(1); 
+        size = 0; 
     }
     
     void add(int num) {
-        vec.push_back(num); 
-        
+        if(num == 0) 
+        {
+            vec = {1}; 
+            size = 0; 
+        }else 
+        {
+            vec.push_back(num*vec[size]); 
+            size += 1; 
+        }
     }
     
     int getProduct(int k) {
-        int n = vec.size(); 
-        ll prod = 1; 
+        // int n = vec.size(); 
+        // ll prod = 1; 
 
-        for(int i=n-1; i>=n-k; i--)
-        {
-            prod *= vec[i]; 
-        }
-        return prod; 
+        // for(int i=n-1; i>=n-k; i--)
+        // {
+        //     prod *= vec[i]; 
+        // }
+        // return prod; 
+        
+
+        if(size < k) return 0; 
+         
+        return (vec[size]/vec[size-k]); 
+        
     }
 };
 
