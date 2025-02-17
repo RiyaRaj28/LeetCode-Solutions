b1df0530f66bab68e23135e1fbfa6243766fa034
Riya Raj <rrajj7674@gmail.com>
2025-02-17 22:08:46 +0530

    Time: 4 ms (54.86%), Space: 124.8 MB (39.12%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -23,7 +23,28 @@ public:
             curr = front; 
         }
         return prev; 
+
+
     }
+
+    ListNode* reverse(ListNode* head)
+{
+	ListNode* prev = NULL; 
+	ListNode* curr = head; 
+
+	while(curr != NULL)
+	{
+		ListNode* front = curr -> next; 
+		curr -> next = prev; 
+		
+		prev = curr; 
+		curr = front; 
+	} 
+
+	return prev; 
+} 
+
+
     bool isPalindrome(ListNode* head) {
         //brute : stack ds
         // stack <int> st;
@@ -43,32 +64,108 @@ public:
         // return true; 
 
         //optimal : reverse half list and check in one traversal 
-        ListNode *slow = head, *fast= head;
-        //we want the first mid elem so we stop fast at 2nd last in case of even num of elem thus  fast=fast->next
+        // ListNode *slow = head, *fast= head;
+        // //we want the first mid elem so we stop fast at 2nd last in case of even num of elem thus  fast=fast->next
         
-        if(head==NULL || head->next == NULL) return true; 
-        while(fast->next!=NULL && fast->next->next!=NULL)
-        {
-            slow = slow->next; 
-            fast = fast->next->next; 
-        }
+        // if(head==NULL || head->next == NULL) return true; 
+        // while(fast->next!=NULL && fast->next->next!=NULL)
+        // {
+        //     slow = slow->next; 
+        //     fast = fast->next->next; 
+        // }
 
-        //slow is at first mid
-        ListNode* newHead = reverseLL(slow->next); 
-        ListNode *first=head, *second = newHead, *evenHead = newHead; 
+        // //slow is at first mid
+        // ListNode* newHead = reverseLL(slow->next); 
+        // ListNode *first=head, *second = newHead, *evenHead = newHead; 
 
-        while(second!=NULL)
-        {
-            if((first->val) != (second->val)) 
-            { 
-                reverseLL(evenHead);
-                return false; 
-            }
-            first = first->next; 
-            second = second->next; 
-        }
+        // while(second!=NULL)
+        // {
+        //     if((first->val) != (second->val)) 
+        //     { 
+        //         reverseLL(evenHead);
+        //         return false; 
+        //     }
+        //     first = first->next; 
+        //     second = second->next; 
+        // }
         
-        reverseLL(evenHead);
-        return true; 
+        // reverseLL(evenHead);
+        // return true; 
+
+
+
+
+
+
+
+
+    // ListNode* slow = head; 
+	// ListNode* fast = head;
+
+	// while(fast != NULL && fast -> next != NULL) 
+	// {
+	// 	slow = slow -> next; 	
+	// 	fast = fast -> next -> next; 
+	// } 
+
+	// // slow is at middle of the linkedList 
+	// ListNode* newHead = reverse(slow->next); 
+
+
+
+
+	// // slow->next = NULL; 
+
+	// ListNode* first = head; 
+	// ListNode* second = newHead; 
+
+	// while(second != NULL)
+	// {
+	// 	if(first -> val != second -> val) return false; 
+	// 	first = first -> next; 
+	// 	second = second -> next; 
+	// } 
+
+	// return true; 
+
+    stack<int>st; 
+    ListNode* fast = head; 
+    ListNode* slow = head; 
+
+while(fast != NULL && fast -> next != NULL)
+{
+	fast = fast -> next -> next; 
+	slow = slow -> next; 
+}
+
+
+
+while(slow != NULL)
+{
+	st.push(slow->val); 
+	slow = slow -> next; 
+}
+
+
+
+
+
+ListNode* temp = head; 
+while(!st.empty())
+{
+	if(st.top() != temp -> val)
+{
+ 	return false; 
+
+}	
+	
+	st.pop(); 
+temp = temp-> next;
+	
+}
+
+return true; 
+
+
     }
 };
