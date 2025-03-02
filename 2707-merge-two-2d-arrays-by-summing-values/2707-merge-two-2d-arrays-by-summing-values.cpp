81ba214f53e18e5cf1a1a393091fe123ca15f471
Riya Raj <rrajj7674@gmail.com>
2025-03-02 14:10:18 +0530

    Time: 0 ms (100%), Space: 14.6 MB (95.86%) - LeetHub

--- 2707-merge-two-2d-arrays-by-summing-values/2707-merge-two-2d-arrays-by-summing-values.cpp
+++ 2707-merge-two-2d-arrays-by-summing-values/2707-merge-two-2d-arrays-by-summing-values.cpp
@@ -2,22 +2,57 @@ class Solution {
 public:
     vector<vector<int>> mergeArrays(vector<vector<int>>& nums1, vector<vector<int>>& nums2) 
     {
-        map<int,int>mpp; 
+        // map<int,int>mpp; 
 
         vector<vector<int>>ans; 
-        for(int i=0; i<nums1.size(); i++)
+        // for(int i=0; i<nums1.size(); i++)
+        // {
+        //     mpp[nums1[i][0]] = nums1[i][1]; 
+        // }
+
+        // for(int i=0; i<nums2.size(); i++)
+        // {
+        //     mpp[nums2[i][0]] += nums2[i][1]; 
+        // }
+
+        // for(auto it : mpp)
+        // {
+        //     ans.push_back({it.first, it.second}); 
+        // }
+
+        // return ans; 
+
+        int i = 0, j = 0; 
+
+        while(i<nums1.size() && j<nums2.size())
         {
-            mpp[nums1[i][0]] = nums1[i][1]; 
+            if(nums1[i][0] == nums2[j][0]) 
+            {
+                ans.push_back({nums1[i][0], nums1[i][1]+nums2[j][1]}); 
+                i++, j++; 
+            } 
+            else if(nums1[i][0] < nums2[j][0]) 
+            {
+                ans.push_back({nums1[i][0], nums1[i][1]}); 
+                i++; 
+            } 
+            else 
+            {
+                ans.push_back({nums2[j][0], nums2[j][1]}); 
+                j++; 
+            }
         }
 
-        for(int i=0; i<nums2.size(); i++)
+        while(i<nums1.size())
         {
-            mpp[nums2[i][0]] += nums2[i][1]; 
+            ans.push_back({nums1[i][0], nums1[i][1]}); 
+            i++; 
         }
 
-        for(auto it : mpp)
+        while(j < nums2.size())
         {
-            ans.push_back({it.first, it.second}); 
+            ans.push_back({nums2[j][0], nums2[j][1]}); 
+            j++; 
         }
 
         return ans; 
