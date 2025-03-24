b76d76237bc1511d138a0427ec38eda5f48a07e2
Riya Raj <rrajj7674@gmail.com>
2025-03-24 19:15:52 +0530

    Time: 2645 ms (5.1%), Space: 637.2 MB (5.61%) - LeetHub

--- 3430-count-days-without-meetings/3430-count-days-without-meetings.cpp
+++ 3430-count-days-without-meetings/3430-count-days-without-meetings.cpp
@@ -10,39 +10,75 @@ public:
     {
         sort(meetings.begin(), meetings.end(), comp); 
         int n = meetings.size(); 
-        vector<vector<int>>temp; 
-        temp.push_back(meetings[0]); 
-        for(int i=1; i<n; i++)
-        {
-            vector<int> prev = temp.back(); 
-            vector<int> curr = meetings[i];
-            if(curr[0] <= prev[1])
-            {
-                temp[temp.size()-1][1] = max(prev[1], curr[1]); 
-            }
-            else
-            {
-                temp.push_back(curr); 
-            }
-        }
-
-        // vector<int>hash(days+1, 0); 
-        int total = 0; 
-
-        for(int i=0; i<temp.size(); i++)
-        {
-            int l = temp[i][0]; 
-            int r = temp[i][1]; 
-            total += r-l+1;  
-        }
-
-        // int cnt = 0; 
-        // for(int i=1; i<=days; i++)
+        // vector<vector<int>>temp; 
+        // temp.push_back(meetings[0]); 
+        // for(int i=1; i<n; i++)
         // {
-        //     if(!hash[i]) cnt++; 
+        //     vector<int> prev = temp.back(); 
+        //     vector<int> curr = meetings[i];
+        //     if(curr[0] <= prev[1])
+        //     {
+        //         temp[temp.size()-1][1] = max(prev[1], curr[1]); 
+        //     }
+        //     else
+        //     {
+        //         temp.push_back(curr); 
+        //     }
         // }
 
-        return days-total; 
+        // int total = 0; 
+
+        // for(int i=0; i<temp.size(); i++)
+        // {
+        //     int l = temp[i][0]; 
+        //     int r = temp[i][1]; 
+        //     total += r-l+1;  
+        // }
+        // return days-total; 
+
+
+    int start = 0, end = 0, total = 0; 
+    for(int i=0; i<n; i++)
+    {
+        start = meetings[i][0]; 
+
+        if(start > end)
+        {
+            total += start - end - 1; 
+            end = meetings[i][1]; 
+        }
+        else
+        {
+            end = max(end, meetings[i][1]); 
+        }
+    }
+
+    if(end < days) total += days - end; 
+    return total; 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         
     }
 };
