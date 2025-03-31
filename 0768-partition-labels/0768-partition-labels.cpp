e681a43e60df9f8a35bb0c1b36179d4a47b8d946
Riya Raj <rrajj7674@gmail.com>
2025-03-31 14:16:31 +0530

    Time: 0 ms (100%), Space: 8.9 MB (49.46%) - LeetHub

--- 0768-partition-labels/0768-partition-labels.cpp
+++ 0768-partition-labels/0768-partition-labels.cpp
@@ -2,32 +2,73 @@ class Solution {
 public:
     vector<int> partitionLabels(string s) 
     {
-        unordered_map<char, pair<int, int>>mpp; 
+        // unordered_map<char, pair<int, int>>mpp; 
+        // for(int i=0; i<s.length(); i++)
+        // {
+        //     if(mpp.find(s[i]) == mpp.end()) mpp[s[i]] = {i, i}; 
+        //     else mpp[s[i]].second = i; 
+        // }
+
+        // // for(auto it : mpp) cout << it.first << " -> " << it.second.first << " " << it.second.second << endl; 
+
+        // int start = mpp[s[0]].first, end = mpp[s[0]].second; 
+
+        // int n = s.length(); 
+        // vector<int>ans;
+        // while(start < n && end < n)
+        // {
+        //     for(int i=start+1; i<end; i++)
+        //     {
+        //         end = max(end, mpp[s[i]].second);
+        //     }
+        //     ans.push_back(end-start+1); 
+
+        //     start = end+1; 
+        //     end = mpp[s[start]].second; 
+        // }
+
+        // return ans; 
+
+
+
+
+
+
+        unordered_map<char, int>mpp; 
+        for(int i=0; i<s.length(); i++) mpp[s[i]] = i; 
+
+        int st = 0, e = 0; 
+        vector<int>ans; 
         for(int i=0; i<s.length(); i++)
         {
-            if(mpp.find(s[i]) == mpp.end()) mpp[s[i]] = {i, i}; 
-            else mpp[s[i]].second = i; 
-        }
+            e = max(e, mpp[s[i]]); 
 
-        // for(auto it : mpp) cout << it.first << " -> " << it.second.first << " " << it.second.second << endl; 
-
-        int start = mpp[s[0]].first, end = mpp[s[0]].second; 
-
-        int n = s.length(); 
-        vector<int>ans;
-        while(start < n && end < n)
-        {
-            for(int i=start+1; i<end; i++)
+            if(i == e) 
             {
-                end = max(end, mpp[s[i]].second);
+                ans.push_back(e-st+1); 
+                st = e+1; 
             }
-            ans.push_back(end-start+1); 
-
-            start = end+1; 
-            end = mpp[s[start]].second; 
         }
-
         return ans; 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         
     }
 };
