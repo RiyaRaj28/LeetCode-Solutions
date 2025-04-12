6b11a161a8f6e74fc8f1606bde0d67e12a98eae2
Riya Raj <rrajj7674@gmail.com>
2025-04-12 07:33:14 +0530

    Time: 135 ms (18.31%), Space: 192.5 MB (14.85%) - LeetHub

--- 3797-design-spreadsheet/3797-design-spreadsheet.cpp
+++ 3797-design-spreadsheet/3797-design-spreadsheet.cpp
@@ -1,96 +1,123 @@
 class Spreadsheet {
 public:
-    map<char, vector<int>> mpp;
-    int rows;
+    // map<char, vector<int>> mpp;
+    // int rows;
+
+    vector<vector<int>>spr; 
 
     Spreadsheet(int rows) {
-        this->rows = rows;
+        spr = vector<vector<int>>(rows+1, vector<int>(26)); 
+        
     }
 
-    void setCell(string cells, int value) {
-        char cell = cells[0];
-        string cell_no; 
-        for(int i=1; i<cells.size(); i++)
-        {
-            cell_no += cells[i]; 
-        }
-        int cell_num = stoi(cell_no); 
+    void setCell(string cell, int value) {
+        int col = cell[0] - 'A';
+        // string cell_no; 
+        // for(int i=1; i<cells.size(); i++)
+        // {
+        //     cell_no += cells[i]; 
+        // }
+        // int cell_num = stoi(cell_no); 
 
-        if(mpp.find(cell) == mpp.end())  mpp[cell].resize(rows+1, 0); 
-        mpp[cell][cell_num] = value; 
+        int row = stoi(cell.substr(1)); 
+        spr[row][col] = value; 
+
+        // if(mpp.find(cell) == mpp.end())  mpp[cell].resize(rows+1, 0); 
+        // mpp[cell][cell_num] = value; 
     }
 
-    void resetCell(string cells) {
-        char cell = cells[0];
-        string cell_no; 
+    void resetCell(string cell) {
+        // int col = cell[0] - 'A';
+        // string cell_no; 
 
-            for(int i=1; i<cells.size(); i++)
-            {
-                cell_no += cells[i]; 
-            }
-            int cell_num = stoi(cell_no); 
+        //     for(int i=1; i<cells.size(); i++)
+        //     {
+        //         cell_no += cells[i]; 
+        //     }
+        //     int cell_num = stoi(cell_no); 
+        // int row = stoi(cell.substr(1)); 
 
-        if (mpp.find(cell) != mpp.end() && mpp[cell].size() > cell_num) { 
-            mpp[cell][cell_num] = 0;  
-        }
 
+        // if (mpp.find(cell) != mpp.end() && mpp[cell].size() > cell_num) { 
+        //     mpp[cell][cell_num] = 0;  
+        // }
+
+        setCell(cell, 0); 
+    }
+
+    int get_val(string cell)
+    {
+        if(isdigit(cell[0])) return stoi(cell); 
+
+        int col = cell[0] - 'A'; 
+        int row = stoi(cell.substr(1)); 
+
+        return spr[row][col]; 
     }
 
     int getValue(string formula) {
-        string first, second;
-        int pind = 0;
+        // string first, second;
+        // int pind = 0;
 
-        for (int i = 1; i < formula.length(); i++) {
-            if (formula[i] != '+') first += formula[i];
-            else {
-                pind = i;
-                break;
-            }
-        }
+        // for (int i = 1; i < formula.length(); i++) {
+        //     if (formula[i] != '+') first += formula[i];
+        //     else {
+        //         pind = i;
+        //         break;
+        //     }
+        // }
 
-        for (int i = pind + 1; i < formula.length(); i++) {
-            second += formula[i];
-        }
+        // for (int i = pind + 1; i < formula.length(); i++) {
+        //     second += formula[i];
+        // }
 
-        int fval = 0, sval = 0;
-        if(isalpha(first[0]))
-        {   
-            char cell = first[0];
-            string cell_no; 
+        // int fval = 0, sval = 0;
+        // if(isalpha(first[0]))
+        // {   
+        //     char cell = first[0];
+        //     string cell_no; 
 
-            for(int i=1; i<first.size(); i++)
-            {
-                cell_no += first[i]; 
-            }
-            int cell_num = stoi(cell_no); 
+        //     for(int i=1; i<first.size(); i++)
+        //     {
+        //         cell_no += first[i]; 
+        //     }
+        //     int cell_num = stoi(cell_no); 
 
-            if (mpp.find(cell) != mpp.end() && mpp[cell].size() > cell_num)  
-                fval = mpp[cell][cell_num];  
-            else 
-                fval = 0;      
-        }
-        else 
-            fval = stoi(first);  
+        //     if (mpp.find(cell) != mpp.end() && mpp[cell].size() > cell_num)  
+        //         fval = mpp[cell][cell_num];  
+        //     else 
+        //         fval = 0;      
+        // }
+        // else 
+        //     fval = stoi(first);  
         
-        // Convert second operand
-        if(isalpha(second[0]))
-            {
-                char cell = second[0];
-                string cell_no; 
-            for(int i=1; i<second.size(); i++)
-            {
-                cell_no +=  second[i]; 
-            }
-            int cell_num = stoi(cell_no); 
+        // // Convert second operand
+        // if(isalpha(second[0]))
+        //     {
+        //         char cell = second[0];
+        //         string cell_no; 
+        //     for(int i=1; i<second.size(); i++)
+        //     {
+        //         cell_no +=  second[i]; 
+        //     }
+        //     int cell_num = stoi(cell_no); 
 
-                if (mpp.find(cell) != mpp.end() && mpp[cell].size() > cell_num)  
-                    sval = mpp[cell][cell_num];  
-                else sval = 0; 
-        }
-        else
-            sval = stoi(second);
+        //         if (mpp.find(cell) != mpp.end() && mpp[cell].size() > cell_num)  
+        //             sval = mpp[cell][cell_num];  
+        //         else sval = 0; 
+        // }
+        // else
+        //     sval = stoi(second);
 
-        return fval + sval;
+        // return fval + sval;
+
+        formula = formula.substr(1); 
+        int plus_ind = formula.find('+'); 
+
+        return get_val(formula.substr(0, plus_ind)) + get_val(formula.substr(plus_ind+1)); 
+
+
+
     }
 };
 
