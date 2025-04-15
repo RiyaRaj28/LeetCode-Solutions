91bc84a9918c5f6f3047e20d7f0258db34f80139
Riya Raj <rrajj7674@gmail.com>
2025-04-15 07:42:00 +0530

    Time: 2120 ms (53.4%), Space: 98.3 MB (61.38%) - LeetHub

--- 3820-number-of-unique-xor-triplets-ii/3820-number-of-unique-xor-triplets-ii.cpp
+++ 3820-number-of-unique-xor-triplets-ii/3820-number-of-unique-xor-triplets-ii.cpp
@@ -1,34 +1,68 @@
 class Solution {
 public: 
     int uniqueXorTriplets(vector<int>& nums) {
-    int xorpairs[2048] = {0};
+    // int xorpairs[2048] = {0};
 
-    int n = nums.size(); 
+    // int n = nums.size(); 
+    // unordered_set<int>st; 
+
+    // for(int k=0; k<n; k++)
+    // {
+    //     for(int i=0; i<=k; i++)
+    //     {
+    //             xorpairs[nums[i] ^ nums[k]]++; 
+    //     }
+
+    //     for(int j=0; j<=2047; j++)
+    //     {
+    //         int xorval;
+    //         if(xorpairs[j]>0) 
+    //         {
+    //             xorval = j^nums[k]; 
+    //             st.insert(xorval); 
+    //         }
+    //     }
+
+
+    // }
+
+    // return st.size();  
+
+
     unordered_set<int>st; 
-
-    for(int k=0; k<n; k++)
+    int n = nums.size(); 
+    if(n==1 || n==2) return n;
+    for(int i=0; i<n; i++)
     {
-        for(int i=0; i<=k; i++)
+        for(int j=i+1; j<n; j++)
         {
-                // for (int j = i; j <= k; j++) {
-                //     xorpairs[nums[i] ^ nums[j]]++;
-                // }
-                xorpairs[nums[i] ^ nums[k]]++; 
+            st.insert(nums[i]^nums[j]); 
         }
-        
-        for(int j=0; j<=2047; j++)
+    }
+    unordered_set<int>s; 
+
+    
+        for(auto num : st)
         {
-            int xorval;
-            if(xorpairs[j]>0) 
-            {
-                xorval = j^nums[k]; 
-                st.insert(xorval); 
-            }
+            for(int i=0; i<n; i++)
+        {   
+            s.insert(num^nums[i]); 
         }
-
-
     }
 
-    return st.size();  
+    return s.size(); 
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 };
