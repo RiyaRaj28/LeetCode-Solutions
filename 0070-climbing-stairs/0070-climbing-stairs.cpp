612aa8d38305dee12d26369ee7f8fe501610bc2a
Riya Raj <rrajj7674@gmail.com>
2025-07-03 22:09:54 +0530

    Time: 0 ms (100%), Space: 8.5 MB (51.69%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -72,27 +72,70 @@ public:
 
 
 
-    int solve(int n, vector<int>&dp)
-    {
-        if(n == 0 ) return 1; 
-        if(n<0) return 0; 
+    // int solve(int n, vector<int>&dp)
+    // {
+    //     if(n == 0 ) return 1; 
+    //     if(n<0) return 0; 
 
-        if(dp[n] != -1) return dp[n]; 
+    //     if(dp[n] != -1) return dp[n]; 
 
-        int one_step = solve(n-1, dp); 
-        int two_step = solve(n-2, dp); 
+    //     int one_step = solve(n-1, dp); 
+    //     int two_step = solve(n-2, dp); 
 
-        return dp[n] = one_step + two_step; 
+    //     return dp[n] = one_step + two_step; 
         
-    }
+    // }
+
+    // int climbStairs(int n) 
+    // {
+    //     vector<int>dp(n+1, -1); 
+    //     dp[0] = 0; 
+
+    //     solve(n, dp); 
+    //     return dp[n]; 
+
+    // }
+
+
+
+
+
+
+
+    // int solve(int n, vector<int>&dp)
+    // {
+    //     if(n == 0 ) return 1; 
+    //     if(n<0) return 0; 
+
+    //     if(dp[n] != -1) return dp[n]; 
+
+    //     int one_step = solve(n-1, dp); 
+    //     int two_step = solve(n-2, dp); 
+
+    //     return dp[n] = one_step + two_step; 
+        
+    // }
 
     int climbStairs(int n) 
     {
-        vector<int>dp(n+1, -1); 
-        dp[0] = 0; 
+        vector<int>dp(n+1, 0);
+        if(n== 1) return 1; 
+        if(n==2) return 2; 
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 2; 
 
-        solve(n, dp); 
+        for(int i=3; i<=n; i++)
+        {
+            // cout << dp[i-1] << endl;
+            // cout << dp[i-2] << endl;
+            dp[i] = dp[i-1] + dp[i-2];
+        } 
+        // cout << dp[n] << endl; 
         return dp[n]; 
+
+
+
 
     }
 
