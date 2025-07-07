80542f5e1a98c0eb3f6d056477f241578e26afc7
Riya Raj <rrajj7674@gmail.com>
2025-07-07 12:29:29 +0530

    Time: 19 ms (82.22%), Space: 60.2 MB (77.36%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -1,135 +1,35 @@
-// class Solution {
-// public:
-
-//     bool linear_search(int num, vector<int>&nums)
-//     {
-//         for(int i=0; i<nums.size(); i++)
-//         {
-//             if(nums[i] == num) return true; 
-//         }
-//         return false; 
-//     }
-//     int longestConsecutive(vector<int>& nums) 
-//     {
-//         //we have to find the next greater element by 
-//         //linear search, need to find x+1 in the array in 
-//         // the reamining elems from the current one 
-
-//         //be confident 
-
-//         int maxi = 0; 
-
-//         for(int i=0; i<nums.size(); i++)
-//         {
-//             int el = nums[i];
-//             int cnt = 1; 
-
-//             while(linear_search(el+1, nums) == true)
-//             {
-//                 el = el+1; 
-//                 cnt += 1; 
-//             }
-
-//             maxi = max(maxi, cnt);
-//         }
-//         return maxi; 
-        
-//     }
-// };
-
-
-// class Solution {
-// public:
-//     int longestConsecutive(vector<int>& nums) 
-//     {
-//         //we have to find the next greater element by 
-//         //linear search, need to find x+1 in the array in 
-//         // the reamining elems from the current one 
-
-//         //be confident 
-//         // we will keep a variable last smallest 
-//         // and we will check if arr[i]-1 == last smallest, then inc count 
-
-//         int last_smallest = INT_MIN; 
-//         int cnt = 1;
-//         int maxi = 0; 
-//         sort(nums.begin(), nums.end());
-
-//         for(int i=0; i<nums.size(); i++)
-//         {
-//             if(nums[i]-1 == last_smallest)
-//             {
-//                 last_smallest = nums[i];
-//                 cnt += 1; 
-//             }
-//             else if(nums[i] != last_smallest)
-//             {
-//                 last_smallest = nums[i];
-//                 cnt = 1; 
-//             }
-//             maxi = max(maxi, cnt);
-//         }
-//         return maxi; 
-//     }
-// };
-
-
-
-
-
-
-
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) 
     {
-        // insert elements in a set and then perform functions in that 
+        if(nums.size() == 0) return 0; 
+        sort(nums.begin(), nums.end()); 
+
         unordered_set<int>st; 
-
-        for(int i=0; i<nums.size(); i++)
+        int cnt = 1; 
+        int maxcnt = 1; 
+        for(int i=1; i<nums.size(); i++)
         {
-            st.insert(nums[i]); 
-        }
-        int maxlen = 0; 
+            // if(st.find(nums[i] - 1) != st.end())
+            // {
+            //     cnt += 1; 
+            //     maxcnt = max(maxcnt, cnt);
+            // }
+            // else cnt = 1; 
 
-        for(auto it : st)
-        {
-            if(st.find(it-1) == st.end())
+            // st.insert(nums[i]); 
+            if(nums[i] == nums[i-1]) continue; 
+
+            if(nums[i] - 1 == nums[i-1]) 
             {
-                int cnt = 1; 
-                int x = it; 
-                while(st.find(x+1) != st.end())
-                {
-                    cnt++; 
-                    x += 1; 
-                }
-                maxlen = max(maxlen, cnt);
+                cnt++; 
+                maxcnt = max(maxcnt, cnt); 
             }
+            else cnt = 1; 
+
+
         }
-     
-        return maxlen; 
+        return maxcnt; 
+        
     }
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
