62ae2da8a68c39b152acc3fa961e903a56a3013d
Riya Raj <rrajj7674@gmail.com>
2025-07-09 14:27:00 +0530

    Time: 44 ms (80%), Space: 29 MB (90.28%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -2,54 +2,98 @@ class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) 
     {
-        // vector<vector<int>>ans; 
-        // set<vector<int>>st;
+        // map : sumofpairs -> {i,j} 
+        // search for 0 - curr in map 
+        // return i,j,k 
+        // set -> unique elements? 
 
-        int n = nums.size(); 
-        sort(nums.begin(), nums.end()); 
+        // unordered_map<int, vector<pair<int, int>>>mpp; 
 
-        // for(int i=0; i<n-2; i++)
+        // set<vector<int>>st; 
+
+        // int n = nums.size();
+        // for(int i=0; i<n; i++)
         // {
-        //     for(int j=i+1; j<n-1; j++)
+        //     for(int j=i+1; j<n; j++)
         //     {
-        //         for(int k=n-1; k>j; k--)
+        //         if(i!=j)
         //         {
-        //             int sum = nums[i] + nums[j] + nums[k]; 
-        //             if(sum == 0 && i!=j && i!=k && j!=k)
-        //             {
-        //                 st.insert({nums[i],nums[j],nums[k]}); 
-        //             }
+
+        //             mpp[nums[i]+nums[j]].push_back({i,j}); 
         //         }
         //     }
         // }
+        
+        // for(int i=0; i<n; i++)
+        // {
+        //     int tofind = 0 - nums[i]; 
+
+        //     if(mpp.find(tofind) != mpp.end())
+        //     {
+        //         for(int p=0; p<mpp[tofind].size(); p++)
+        //         {
+        //         int j = mpp[tofind][p].first; 
+        //         int k = mpp[tofind][p].second; 
+
+        //         if(i!= j && i!=k)
+        //         {
+        //             vector<int>temp = {nums[i],nums[j],nums[k]}; 
+        //             sort(temp.begin(), temp.end()); 
+
+        //             st.insert(temp); 
+        //         }
+        //         }
+        //     }
+        // }
+
+        // vector<vector<int>>ans; 
+
+        // // while(!st.empty())
+        // // {
+        // //     vector<int>t = st.top(); 
+        // //     ans.push_back(t);
+        // //     st.pop(); 
+        // // }
+
+        // for(auto it : st)
+        // {
+        //     vector<int>t = it; 
+        //     ans.push_back(t); 
+        // }
+
+        // return ans; 
+
+        
         vector<vector<int>>ans; 
-
-
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
         for(int i=0; i<n; i++)
         {
-            if(i!=0 && nums[i] == nums[i-1]) continue; 
-            
+            if(i!= 0 && nums[i] == nums[i-1]) continue; 
+
             int j = i+1; 
             int k = n-1; 
-        
+
             while(j<k)
             {
                 int sum = nums[i] + nums[j] + nums[k]; 
-                if(sum < 0 && j<n-1) j++; 
-                else if(sum > 0 && k>j) k--;
-                else
+
+                if(j<n-1 && sum < 0) j++; 
+                else if(k>j && sum > 0) k--;
+                else 
                 {
-                    ans.push_back({nums[i], nums[j], nums[k]});
+                    ans.push_back({nums[i], nums[j], nums[k]}); 
                     j++;
                     k--;
 
-                    while(j<k && nums[j] == nums[j-1]) j++; 
-                    while(j<k && nums[k] == nums[k+1]) k--; 
+                    while(j<k && nums[j]==nums[j-1]) j++; 
+                    while(j<k && nums[k]==nums[k+1] ) k--; 
                 }
             }
         }
 
         return ans; 
-        
+
     }
+
 };
