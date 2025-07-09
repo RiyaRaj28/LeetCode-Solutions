39ab3be0ab280d96dd963f8bc3eb9c57756c7973
Riya Raj <rrajj7674@gmail.com>
2025-07-09 18:58:56 +0530

    Time: 4 ms (30.52%), Space: 64.6 MB (14.84%) - LeetHub

--- 1254-deepest-leaves-sum/1254-deepest-leaves-sum.cpp
+++ 1254-deepest-leaves-sum/1254-deepest-leaves-sum.cpp
@@ -31,7 +31,30 @@ public:
 
     int deepestLeavesSum(TreeNode* root) {
 
-        func(root, 1);
+        // func(root, 1);
+        // return sum; 
+
+        queue<pair<TreeNode*, int>>q; 
+        q.push({root, 1}); 
+        int sum = 0; 
+
+        while(!q.empty())
+        {
+            sum = 0; 
+            int size = q.size(); 
+
+            while(size--)
+            {
+                TreeNode* node = q.front().first; 
+                int level = q.front().second; 
+                q.pop();
+
+                sum += node->val; 
+                if(node->left) q.push({node->left, level+1}); 
+                if(node->right) q.push({node->right, level+1}); 
+            }
+        }
+
         return sum; 
     }
 };
