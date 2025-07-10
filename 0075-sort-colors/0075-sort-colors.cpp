61df15a737b6bce8a51a7b69798baa089ee4b473
Riya Raj <rrajj7674@gmail.com>
2025-07-10 15:16:07 +0530

    Time: 0 ms (100%), Space: 11.6 MB (49.04%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,33 +1,89 @@
 class Solution {
 public:
+
+    // void merge(vector<int>&nums, int low, int mid, int high)
+    // {
+    //     if(low >= high) return; 
+    //     vector<int>temp; 
+
+    //     int left = low, right = mid+1; 
+    //     while(left<=mid && right<=high)
+    //     {
+    //         if(nums[left] <= nums[right])
+    //         {
+    //             temp.push_back(nums[left]); 
+    //             left++; 
+    //         }
+    //         else
+    //         {
+    //             temp.push_back(nums[right]);
+    //             right++; 
+    //         }
+    //     }
+
+    //     while(left <= mid)
+    //     {
+    //         temp.push_back(nums[left]);
+    //     }
+
+    //     while(right<=high)
+    //     {
+    //         temp.push_back(nums[right]); 
+    //     }
+
+    //     for(int i=low; i<=high; i++)
+    //     {
+    //         nums[i] = temp[i-low]; 
+    //     }
+    // }
+
+    // void mergeSort(vector<int>&nums, int low, int high)
+    // {
+    //     if(low >= high) return; 
+    //     int mid = low + (high-low)/2; 
+
+    //     mergeSort(nums, low, mid); 
+    //     mergeSort(nums, mid+1, high); 
+    //     merge(nums, low, mid, high); 
+    // }
+
+    // void sortColors(vector<int>& nums) 
+    // {
+    //    int n = nums.size(); 
+
+    //    mergeSort(nums, 0, n-1); 
+    // //    return nums; 
+    // }
+
+
+
+
+
     void sortColors(vector<int>& nums) 
     {
-        int n = nums.size(); 
-        int c0 = 0, c1 = 0, c2 = 0; 
+       int n = nums.size(); 
+       int mid = 0; 
+       int low = 0, high = n-1; 
 
-        for(int i=0; i<n; i++)
-        {
-            if(nums[i] == 0) c0++;
-            else if(nums[i] == 1) c1++;
-            else c2++;
-        }
+       while(mid<=high)
+       {
+            if(nums[mid] == 0)
+            {
+                swap(nums[mid], nums[low]); 
+                mid++;
+                low++;
+            }
+            else if(nums[mid] == 1)
+            {
+                mid++;
+            }
+            else
+            {
+                swap(nums[mid], nums[high]);
+                high--;
+            }
 
-        for(int i=0; i<c0; i++)
-        {
-            nums[i] = 0; 
-
-        }
-        for(int i=c0; i<c0+c1; i++)
-        {
-            nums[i] = 1; 
-        }
-        for(int i=c0+c1; i<n; i++)
-        {
-            nums[i] = 2; 
-        }
-
-        // return nums; 
-
-        
+       }
+   
     }
 };
