1c7621453a42450aebf9ec839249ae252677a2ca
Riya Raj <rrajj7674@gmail.com>
2025-07-10 13:50:44 +0530

    Time: 11 ms (33.04%), Space: 139 MB (27.01%) - LeetHub

--- 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.cpp
+++ 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.cpp
@@ -4,24 +4,85 @@ public:
     {
         //equal pos, neg 
         // maintain pos and neg indices 
-        int n = nums.size(); 
-        vector<int>ans(n, 0);
+        // int n = nums.size(); 
+        // vector<int>ans(n, 0);
 
-        int negi = 1, posi = 0; 
+        // int negi = 1, posi = 0; 
         
+        // for(int i=0; i<n; i++)
+        // {
+        //     if(nums[i] > 0) 
+        //     {
+        //         ans[posi] = nums[i];
+        //         posi += 2;
+        //     }
+        //     else 
+        //     {
+        //         ans[negi] = nums[i];
+        //         negi += 2;
+        //     }
+        // }
+        // return ans; 
+
+
+        // feed according to whichever is lesser and whichever is more 
+
+        int n = nums.size(); 
+        int pos = 0, neg = 0; 
+        vector<int>posa; 
+        vector<int>nega; 
         for(int i=0; i<n; i++)
         {
             if(nums[i] > 0) 
             {
-                ans[posi] = nums[i];
-                posi += 2;
+                posa.push_back(nums[i]); 
+                pos++; 
             }
             else 
             {
-                ans[negi] = nums[i];
-                negi += 2;
+                nega.push_back(nums[i]); 
+                neg++; 
             }
         }
+
+        // if neg > pos 
+        // fill till the pos indexes, then we go to the neg indexes 
+        vector<int>ans(n); 
+
+
+        if(pos>=neg)
+        {
+            // ans[2*i] = pos[i]
+            // ans[2*i + 1] = neg[i]
+            int ind = 0; 
+            for(ind=0; ind<neg; ind++)
+            {
+                ans[2*ind] = posa[ind]; 
+                ans[2*ind + 1] = nega[ind]; 
+            }
+
+            for(int j=neg; j<pos; j++)
+            {
+                ans[ind++] = posa[j]; 
+            }
+
+        }
+        else
+        {
+            int ind = 0; 
+            for(ind=0; ind<pos; ind++)
+            {
+                ans[2*ind] = posa[ind]; 
+                ans[2*ind + 1] = nega[ind]; 
+            }
+
+            for(int j=pos; j<neg; j++)
+            {
+                ans[ind++] = nega[j]; 
+            }
+
+        }
+
         return ans; 
     }
 };
