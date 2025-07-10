928673473dca5729850d8eea1d415dfe1ca6f61c
Riya Raj <rrajj7674@gmail.com>
2025-07-10 16:47:50 +0530

    Time: 0 ms (100%), Space: 29.6 MB (57.45%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -1,21 +1,17 @@
 class Solution {
 public:
-    void rotatePart(vector <int> &nums, int i, int j)
+    void rotate(vector<int>& nums, int d) 
     {
-        while(i<=j)
-        {
-            int temp = nums[i];
-            nums[i] = nums[j];
-            nums[j] = temp; 
-            i++; 
-            j--; 
-        }
-    }
-    void rotate(vector<int>& nums, int k) {
-        int n = nums.size();
-        k = k % n; 
-        rotatePart(nums,0,n-1);
-        rotatePart(nums,0,k-1);
-        rotatePart(nums,k,n-1);
+        
+        int n = nums.size(); 
+        
+        d = d%n; 
+        reverse(nums.begin(), nums.begin()+n-d);
+        reverse(nums.begin()+n-d, nums.begin()+n); 
+        reverse(nums.begin(), nums.begin()+n); 
+
+        
+
+        
     }
 };
