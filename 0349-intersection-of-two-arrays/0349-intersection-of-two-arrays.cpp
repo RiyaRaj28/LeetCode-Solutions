a597026b0c5d245948696674d66f1a3537f71b75
Riya Raj <rrajj7674@gmail.com>
2025-07-10 16:29:11 +0530

    Time: 7 ms (10.15%), Space: 13.6 MB (100%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
@@ -1,103 +1,31 @@
 class Solution {
 public:
-    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
-    // {
-    //     // vector<int>temp; 
-    //     // int n1 = nums1.size(); 
-    //     // int n2 = nums2.size(); 
-    //     // sort(nums1.begin(), nums1.end()); 
-    //     // sort(nums2.begin(), nums2.end()); 
-
-    //     // for(int i=0; i<n1; i++)
-    //     // {
-    //     //     for(int j=0; j<n2; j++)
-    //     //     {
-    //     //         if(nums1[i] == nums2[j])
-    //     //         {
-    //     //             if(temp.empty() || nums2[j] != temp.back())
-    //     //             temp.push_back(nums1[i]);
-    //     //             break; 
-    //     //         }
-
-    //     //         if(nums2[j] > nums1[i]) break; 
-    //     //     }
-    //     // }
-    //     // return temp; 
-
-
-
-
-
-
-
-
-    //     vector<int>temp; 
-    //     int n1 = nums1.size(); 
-    //     int n2 = nums2.size(); 
-    //     sort(nums1.begin(), nums1.end()); 
-    //     sort(nums2.begin(), nums2.end()); 
-
-    //     int i = 0, j = 0; 
-    //     while(i < n1 && j < n2)
-    //     {
-    //         if(nums1[i] < nums2[j]) i++; 
-    //         else if(nums1[i] > nums2[j]) j++; 
-    //         else 
-    //         {
-    //             if(temp.empty() || temp.back() != nums1[i])
-    //             {
-    //                 temp.push_back(nums1[i]); 
-    //             }
-    //             i++; 
-    //             j++;
-    //         }
-    //     }
-    //     return temp; 
-    // }
-
-
-
-
-    vector<int>temp; 
-    int n = nums1.size(); 
-    int m = nums2.size(); 
-
-    sort(nums1.begin(), nums1.end()); 
-    sort(nums2.begin(), nums2.end()); 
-
-    int i=0, j=0; 
-    while(i<n && j<m)
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2)
     {
-        if(nums1[i] < nums2[j]) i++;
-        else if(nums1[i] > nums2[j]) j++;
-        else
+        int n1 = nums1.size(), n2 = nums2.size(); 
+        vector<int>temp; 
+        sort(nums1.begin(), nums1.end()); 
+        sort(nums2.begin(), nums2.end()); 
+
+        for(int i=0; i<n1; i++)
         {
-            //both equal 
-            if (temp.empty() || nums1[i] != temp.back()) 
+            for(int j=0; j<n2; j++)
             {
-                temp.push_back(nums1[i]);
-                
+                if(nums1[i] == nums2[j])
+                {
+                    if(temp.empty() || nums1[i] != temp.back())
+                    {
+                        temp.push_back(nums1[i]); 
+                    }
+                }
+                else if(nums2[j] > nums1[i])
+                {
+                    break; 
+                }
             }
-            i++;
-            j++; 
         }
+
+        return temp; 
+        
     }
-
-    return temp;
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 };
