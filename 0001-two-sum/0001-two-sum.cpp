4450ed443fda79aa6f7d149edbcf14f06f128c10
Riya Raj <rrajj7674@gmail.com>
2025-07-10 15:19:54 +0530

    Time: 4 ms (57.15%), Space: 15 MB (14.55%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,51 +1,23 @@
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        //m1 - two looops - n2
-        for(int i=0; i<nums.size(); i++)
+    vector<int> twoSum(vector<int>& nums, int target) 
+    {
+        int n = nums.size(); 
+        map<int, int>mpp;
+
+        for(int i=0; i<n; i++)
         {
-            for(int j=i+1; j<nums.size(); j++)
+            int num = nums[i]; 
+            int rem = target - num; 
+
+            if(mpp.find(rem) != mpp.end())
             {
-                if(nums[i]+nums[j]==target)
-                {
-                    return{i,j};
-                }
+                return {i, mpp[rem]};
             }
+
+            mpp[num] = i;
+
         }
-        return {-1, -1}; 
-
-        // m2 - hashing
-        // map<int, int> mpp; 
-        // for(int i=0; i<nums.size(); i++)
-        // {
-        //     int rem = target - nums[i]; 
-        //     if(mpp.find(rem) != mpp.end())
-        //     {
-        //         return {i, mpp[rem]};
-        //     }
-        //     mpp[nums[i]] = i; 
-        // }
-        // return {-1, -1}; 
-
-        //m3 - greedy app only for finding if nums exist or not
-        //will not work since we are sorting the array so   indices   will not be the same
-        // int left=0, right=nums.size()-1;
-        // sort(nums.begin(), nums.end()); 
-
-        // while(left<right)
-        // {
-        //     int sum = nums[left] + nums[right];
-        //     if(sum == target)
-        //     {
-        //         return{left,right};
-        //     }
-
-        //     else if(sum>target) right--;
-        //     else left++;
-        // }
-        // return {-1,-1}; 
-
-
-        
+        return {-1, -1};
     }
 };
