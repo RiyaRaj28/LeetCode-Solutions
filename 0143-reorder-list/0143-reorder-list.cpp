297702f072675d68046513ea3587ef3404ed24c9
Riya Raj <rrajj7674@gmail.com>
2025-07-11 10:49:39 +0530

    Time: 0 ms (100%), Space: 22.9 MB (33.97%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.cpp
+++ 0143-reorder-list/0143-reorder-list.cpp
@@ -12,31 +12,83 @@
  // kal se, ye timer 5 baje start hona chahiye aur raat 9 baje end hona chahiye. 
 class Solution {
 public:
+
+    ListNode* reverse(ListNode* node)
+    {
+        if(!node) return node; 
+
+        ListNode* prev = NULL; 
+
+        while(node)
+        {
+            ListNode* front = node->next; 
+            node->next = prev; 
+            
+
+            prev = node; 
+            node = front; 
+        }
+        return prev;
+    }
+
     void reorderList(ListNode* head) 
     {
-        ListNode* curr = head; 
+        // ListNode* curr = head; 
 
-        while(curr && curr->next)
-        {
-            ListNode* temp = curr->next; 
-            while(temp->next && temp->next->next!=NULL)
-            {
-                temp = temp->next; 
-            }
+        // while(curr && curr->next)
+        // {
+        //     ListNode* temp = curr->next; 
+        //     while(temp->next && temp->next->next!=NULL)
+        //     {
+        //         temp = temp->next; 
+        //     }
 
-            ListNode* tolink = temp->next; 
-            if(!tolink) break; 
+        //     ListNode* tolink = temp->next; 
+        //     if(!tolink) break; 
 
-            temp->next = NULL; 
+        //     temp->next = NULL; 
 
-            tolink->next = curr->next; 
-            curr->next = tolink; 
+        //     tolink->next = curr->next; 
+        //     curr->next = tolink; 
 
-            curr = tolink->next; 
-        }
+        //     curr = tolink->next; 
+        // }
 
         // return head; 
 
+        ListNode* temp = head; 
+
+        ListNode* slow = head, *fast = head; 
+        while(fast && fast->next)
+        {
+            slow = slow->next; 
+            fast = fast->next->next; 
+        }
+
+        ListNode* mid = slow->next; 
+        slow->next = NULL; 
+        ListNode* reversedhead = reverse(mid); 
+
         
+
+        ListNode* left = head->next, *right = reversedhead; 
+
+        while(left && right)
+        {
+            
+            temp->next = right; 
+            right = right->next; 
+            temp = temp->next; 
+
+            temp->next = left; 
+            left = left->next; 
+            temp=temp->next;
+
+        }
+
+        if(left) temp->next = left;
+        if(right) temp->next = right;
+
+        // return dummy->next;
     }
 };
