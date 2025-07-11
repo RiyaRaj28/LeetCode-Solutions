a3279e18baa4be186f532d7a5bbd9b891bb67575
Riya Raj <rrajj7674@gmail.com>
2025-07-12 01:40:02 +0530

    Time: 52 ms (74.14%), Space: 125 MB (43.4%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.cpp
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.cpp
@@ -1,34 +1,43 @@
 class MedianFinder {
-private:
-    priority_queue<int, vector<int>, greater<int>>min_pq;
-    priority_queue<int>max_pq;  
 public:
-    MedianFinder() 
-    {
+    priority_queue<int, vector<int>, greater<int>>min_pq; 
+    priority_queue<int>max_pq; 
+
+    MedianFinder() {
         
     }
     
     void addNum(int num) 
     {
-        if(max_pq.empty() || num < max_pq.top()) max_pq.push(num);
-        else min_pq.push(num);
-
-        if(max_pq.size() > 1 + min_pq.size())
+        if(max_pq.empty() || num < max_pq.top())
         {
-            min_pq.push(max_pq.top());
-            max_pq.pop();
+            max_pq.push(num);
+        }
+        else min_pq.push(num); 
+
+        if(max_pq.size() > min_pq.size() + 1)
+        {
+            min_pq.push(max_pq.top()); 
+            max_pq.pop(); 
         }
         else if(max_pq.size() < min_pq.size())
         {
-            max_pq.push(min_pq.top());
+            max_pq.push(min_pq.top()); 
             min_pq.pop();
         }
     }
     
     double findMedian() 
     {
-        if(max_pq.size() == min_pq.size()) return (double)(max_pq.top() + min_pq.top())/2; 
-        return max_pq.top();
+        if(min_pq.size() == max_pq.size())
+        {
+            return (double)(max_pq.top() + min_pq.top())/2;
+        }
+        else
+        {
+            return max_pq.top();
+        }
+        
     }
 };
 
