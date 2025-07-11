711ba0f27e8353f5e6731f636143b3a824f5d1cc
Riya Raj <rrajj7674@gmail.com>
2025-07-11 18:09:33 +0530

    Time: 14 ms (19.81%), Space: 11 MB (18.58%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -2,18 +2,18 @@ class Solution {
 public:
     int characterReplacement(string s, int k) 
     {
-        int l = 0, r = 0; 
+        int maxfreq = 0; 
+        map<int, int>mpp; 
         int maxlen = 0; 
-        int n = s.length();
-        unordered_map<int, int>mpp; 
 
-        int maxFreq = 0; 
+        int n = s.length(); 
+        int l = 0, r = 0; 
         while(r<n)
         {
             mpp[s[r]]++;
-            maxFreq = max(maxFreq, mpp[s[r]]);
+            maxfreq = max(maxfreq, mpp[s[r]]);
 
-            while( (r - l + 1) - maxFreq > k)
+            if((r-l+1) - maxfreq > k)
             {
                 mpp[s[l]]--;
                 l++;
@@ -22,9 +22,8 @@ public:
             maxlen = max(maxlen, r-l+1);
             r++;
         }
- 
-        return maxlen; 
 
+        return maxlen; 
         
     }
 };
