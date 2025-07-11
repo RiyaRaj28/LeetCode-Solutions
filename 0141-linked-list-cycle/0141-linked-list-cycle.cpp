500fa61f303d3856ab202a3c6ccfb8f7794d1ca9
Riya Raj <rrajj7674@gmail.com>
2025-07-11 09:09:07 +0530

    Time: 12 ms (39.57%), Space: 11.8 MB (79.96%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -8,78 +8,21 @@
  */
 class Solution {
 public:
-    bool hasCycle(ListNode *head) {
-        // //map method - hashing method
-        // // map<ListNode* , int> mpp;
-        // // ListNode* temp = head;
-        // // while(temp)
-        // // {
-        // //     if(mpp.find(temp) != mpp.end())
-        // //         return true; 
-            
-        // //     mpp[temp] = 1;
-        // //     temp = temp->next; 
-        // // }
-        // // return false; 
-
-        // //tortoise-hare method 
-        // ListNode *fast = head, *slow = head;
-        // while(fast != NULL && fast->next!=NULL)
-        // {
-        //     slow = slow->next;
-        //     fast = fast->next->next;
-        //     if(slow == fast)
-        //     {
-        //         return true; 
-        //     }
-        // }
-        // return false; 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        //storing in hashmap 
-        // map<ListNode*, int> mpp; 
-
-        //traverse through the entire ll and insert elements into the hashmap with a value of 1
-        //if before inserting a value in the hashmap it is already found, then return true
-
-        // ListNode* temp = head; 
-        // while(temp!=NULL)
-        // {
-        //     if(mpp.find(temp) != mpp.end()) return true; 
-
-        //     mpp[temp] = temp->val; 
-
-        //     temp = temp->next; 
-        // }
-
-        // return false; 
-
-        //tortoise hare method 
-
+    bool hasCycle(ListNode *head) 
+    {
         ListNode* slow = head, *fast = head; 
-        while(fast!=NULL && fast->next!=NULL)
+
+        while(fast && fast->next)
         {
             slow = slow->next; 
             fast = fast->next->next; 
 
-            if(fast == slow) return true; 
+            if(slow == fast)
+            {
+                return true;
+            }
         }
-
         return false; 
+        
     }
 };
