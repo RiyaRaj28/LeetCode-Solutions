b01071947e38b34fd78cd60f8fb613256c1fe613
Riya Raj <rrajj7674@gmail.com>
2025-07-12 16:28:14 +0530

    Time: 0 ms (100%), Space: 7.9 MB (99.65%) - LeetHub

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
@@ -1,49 +1,59 @@
 class Solution {
 public:
-    // bool check(int ind, string s, int cnt)
-    // {
-    //     if(cnt < 0) return false; 
-    //     if(ind == s.size()) return cnt == 0; 
-    //     if(s[ind] == '(') return check(ind+1, s, cnt+1);
-    //     else if(s[ind] == ')') return check(ind+1, s, cnt-1);
-    //     else 
-    //     {
-    //         bool left = check(ind+1, s, cnt+1);
-    //         bool right = check(ind+1, s, cnt-1);
-    //         bool empty = check(ind+1, s, cnt);
-    //         return left | right | empty;
-    //     }
-    // }
 
-    // bool checkValidString(string s) 
-    // {
-    //     return check(0, s, 0);
-    // }
+    bool check(string s, int ind, int cnt)
+    {
+        if(cnt < 0) return false; 
+        if(ind == s.size()) return cnt == 0; 
 
+        if(s[ind] == '(') return check(s, ind+1, cnt+1);
+        if(s[ind] == ')') return check(s, ind+1, cnt-1);
+        
+        bool left = check(s, ind+1, cnt+1); 
+        bool right = check(s, ind+1, cnt-1); 
+        bool empty = check(s, ind+1, cnt); 
+
+        return left || right || empty; 
+    }
     bool checkValidString(string s) 
     {
-        int min=0, max=0; 
-        for(int i=0; i<s.size(); i++)
+        // left -> cnt++
+        // right -> cnt--
+        // ast -> cnt++ // cnt-- // nothing
+
+        // return check(s, 0, 0); 
+
+        int maxi = 0, mini = 0; 
+
+        int n = s.length(); 
+
+        for(int i=0; i<n; i++)
         {
             if(s[i] == '(')
             {
-                min += 1; 
-                max += 1; 
+                maxi += 1; 
+                mini += 1;
             }
             else if(s[i] == ')')
             {
-                min -= 1; 
-                max -= 1; 
+                maxi -= 1; 
+                mini -= 1; 
             }
-            else 
+            else
             {
-                min -= 1; 
-                max += 1; 
+                maxi += 1; 
+                mini -= 1;
             }
 
-            if(min<0) min = 0; 
-            if(max<0) return false; 
+            if(mini < 0) mini = 0; 
+            if(maxi < 0) return false;
         }
-        return min == 0; 
+
+        return mini == 0; 
+
+
+
+
+        
     }
 };
