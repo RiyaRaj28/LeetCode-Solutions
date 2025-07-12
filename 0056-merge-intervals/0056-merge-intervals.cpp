432049497338d28b22b152ff7fd40821f88fff85
Riya Raj <rrajj7674@gmail.com>
2025-07-12 18:31:17 +0530

    Time: 7 ms (41.95%), Space: 24 MB (31.89%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -5,31 +5,75 @@ public:
         int n = intervals.size(); 
         sort(intervals.begin(),intervals.end()); 
 
-        int ps = intervals[0][0]; 
-        int pe = intervals[0][1]; 
+        // int ps = intervals[0][0]; 
+        // int pe = intervals[0][1]; 
+        // vector<vector<int>>ans; 
+
+        // for(int i=1; i<n; i++)
+        // {
+        //     int ns = intervals[i][0]; 
+        //     int ne = intervals[i][1]; 
+
+        //     if(ns > pe)
+        //     {
+        //         ans.push_back({ps, pe}); 
+        //         ps = ns; 
+        //         pe = ne; 
+        //     }
+        //     else
+        //     {
+        //         ps = min(ps, ns); 
+        //         pe = max(pe, ne); 
+        //     }
+        // }
+
+        // ans.push_back({ps, pe}); 
+
+        // return ans; 
+
+
         vector<vector<int>>ans; 
-
-        for(int i=1; i<n; i++)
+        // int n = intervals.size(); 
+        vector<int>temp = intervals[0]; 
+        for(int i=0; i<n; i++)
         {
-            int ns = intervals[i][0]; 
-            int ne = intervals[i][1]; 
-
-            if(ns > pe)
+            if(intervals[i][0] <= temp[1])
             {
-                ans.push_back({ps, pe}); 
-                ps = ns; 
-                pe = ne; 
+                temp[1] = max(intervals[i][1], temp[1]);
             }
             else
             {
-                ps = min(ps, ns); 
-                pe = max(pe, ne); 
+                ans.push_back(temp); 
+                temp = intervals[i];
             }
         }
 
-        ans.push_back({ps, pe}); 
-
+        ans.push_back(temp); 
         return ans; 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         
     }
 };
