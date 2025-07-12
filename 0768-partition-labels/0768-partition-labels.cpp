d65532cc2b79de570c45464e7450bd3bda92c9fc
Riya Raj <rrajj7674@gmail.com>
2025-07-12 16:14:37 +0530

    Time: 3 ms (29.54%), Space: 8.9 MB (71.18%) - LeetHub

--- 0768-partition-labels/0768-partition-labels.cpp
+++ 0768-partition-labels/0768-partition-labels.cpp
@@ -2,73 +2,29 @@ class Solution {
 public:
     vector<int> partitionLabels(string s) 
     {
-        // unordered_map<char, pair<int, int>>mpp; 
-        // for(int i=0; i<s.length(); i++)
-        // {
-        //     if(mpp.find(s[i]) == mpp.end()) mpp[s[i]] = {i, i}; 
-        //     else mpp[s[i]].second = i; 
-        // }
-
-        // // for(auto it : mpp) cout << it.first << " -> " << it.second.first << " " << it.second.second << endl; 
-
-        // int start = mpp[s[0]].first, end = mpp[s[0]].second; 
-
-        // int n = s.length(); 
-        // vector<int>ans;
-        // while(start < n && end < n)
-        // {
-        //     for(int i=start+1; i<end; i++)
-        //     {
-        //         end = max(end, mpp[s[i]].second);
-        //     }
-        //     ans.push_back(end-start+1); 
-
-        //     start = end+1; 
-        //     end = mpp[s[start]].second; 
-        // }
-
-        // return ans; 
-
-
-
-
-
-
+        int n = s.length(); 
         unordered_map<char, int>mpp; 
-        for(int i=0; i<s.length(); i++) mpp[s[i]] = i; 
 
         int st = 0, e = 0; 
+        for(int i=0; i<n; i++)
+        {
+            mpp[s[i]] = i;
+        }
+
         vector<int>ans; 
-        for(int i=0; i<s.length(); i++)
+
+        for(int i=0; i<n; i++)
         {
             e = max(e, mpp[s[i]]); 
 
-            if(i == e) 
+            if(i == e)
             {
                 ans.push_back(e-st+1); 
                 st = e+1; 
             }
         }
+
         return ans; 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
         
     }
 };
