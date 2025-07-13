db9055e93147443c81df9f3a90ab4defee0afd7c
Riya Raj <rrajj7674@gmail.com>
2025-07-13 09:07:06 +0530

    Time: 3 ms (25.65%), Space: 10.5 MB (97.29%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -17,16 +17,38 @@ public:
         return cnt; 
     }
 
+    int briankerghinan(int n)
+    {
+        int cnt = 0; 
+        while(n)
+        {
+            n = n&(n-1); 
+            cnt++; 
+        }
+
+        return cnt; 
+    }
+
     vector<int> countBits(int n) 
     {
         vector<int>ans(n+1); 
 
         for(int i=0; i<=n; i++)
         {
-            ans[i] = cntbits(i); 
+            // ans[i] = cntbits(i); 
+            ans[i] = briankerghinan(i);
         }
 
         return ans; 
+
+
+
+
+
+
+
+
+
 
         
     }
