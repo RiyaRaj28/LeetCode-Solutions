47d7b63874df0f6cf23ac00eddaac3cf7e57bb45
Riya Raj <rrajj7674@gmail.com>
2025-07-13 07:57:30 +0530

    Time: 4 ms (12.15%), Space: 10.7 MB (58.48%) - LeetHub

---
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int cntbits(int n)
+    {
+        int cnt = 0; 
+
+        while(n>0)
+        {
+            if(n%2 != 0)
+            {
+                cnt += 1; 
+                
+            }
+            n = n/2; 
+        }
+
+        return cnt; 
+    }
+
+    vector<int> countBits(int n) 
+    {
+        vector<int>ans(n+1); 
+
+        for(int i=0; i<=n; i++)
+        {
+            ans[i] = cntbits(i); 
+        }
+
+        return ans; 
+
+        
+    }
+};
