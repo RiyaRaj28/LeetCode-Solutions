2a48d17bff02a5761feefd0c80cea95d286464c5
Riya Raj <rrajj7674@gmail.com>
2025-07-14 21:46:36 +0530

    Time: 324 ms (33.61%), Space: 21.2 MB (47.61%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.cpp
+++ 0127-word-ladder/0127-word-ladder.cpp
@@ -2,49 +2,39 @@ class Solution {
 public:
     int ladderLength(string beginWord, string endWord, vector<string>& wordList) 
     {
-        // push all the words from the wordlist into the set
-        // in the queue, add the first word with the lnth as 1 
-        // then pop the word 
-        // for each char of the given word, 
-        // iterate select the character
-        // iterate from a to z and choose one character 
-        // find that char in the set 
-        // if found, check if its the end word return lnth + 1
-        // else if it is found in the set, add it to the queue and 
-        // remove from the set 
-
-        queue<pair<string, int>>q; 
-        q.push({beginWord, 1}); 
-
         set<string>st; 
-        for(auto it : wordList)
-        {
-            st.insert(it); 
-        }
+        for(auto it: wordList) st.insert(it); 
+
+        queue<pair<int, string>>q; 
+
+        q.push({1, beginWord});
 
         while(!q.empty())
         {
-            string str = q.front().first; 
-            int lnth = q.front().second; 
+            string word = q.front().second; 
+            int step = q.front().first;
             q.pop();
 
-            for(int i=0; i<str.length(); i++)
+            for(int i=0; i<word.length(); i++)
             {
-                char ch = str[i]; 
-                for(char c = 'a'; c <= 'z'; c++)
+                char org = word[i];
+                for(char ch='a'; ch<='z'; ch++)
                 {
-                    str[i] = c; 
-                    if(st.find(str) != st.end())
-                    {
-                        if(str == endWord) return lnth+1; 
+                    word[i] = ch;
 
-                        q.push({str, lnth+1}); 
-                        st.erase(str); 
+                    if(st.find(word) != st.end())
+                    {
+                        if(word == endWord)
+                        {
+                        return step+1;
+                        } 
+                        q.push({step+1, word});
+                        st.erase(word);
                     }
                 }
-                str[i] = ch; 
+                word[i] = org;
             }
         }
-        return 0; 
+        return 0;
     }
 };
