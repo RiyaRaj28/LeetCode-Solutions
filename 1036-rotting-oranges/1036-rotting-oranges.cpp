7f2a25e1c0512c028adad81c7ec8eaa9623de709
Riya Raj <rrajj7674@gmail.com>
2025-07-15 14:19:19 +0530

    Time: 0 ms (100%), Space: 16.7 MB (75.2%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.cpp
+++ 1036-rotting-oranges/1036-rotting-oranges.cpp
@@ -1,62 +1,54 @@
 class Solution {
 public:
-    // void bfs()
     int orangesRotting(vector<vector<int>>& grid) 
     {
-        // bfs - 4 directionally 
-        // rety min time till no cell has a fresh orange 
-
-        // call bfs on the rotten oranges 
-        // we search in the surroundings -> bfs 
-        queue<pair<pair<int, int>, int>>q; 
         int m = grid.size();
-        int n = grid[0].size(); 
-        vector<vector<int>>visited(m, vector<int>(n, 0)); 
+        int n = grid[0].size();
+
+        queue<pair<int, pair<int, int>>>q;
         int fresh = 0; 
 
         for(int i=0; i<m; i++)
         {
             for(int j=0; j<n; j++)
             {
-                if(!visited[i][j] && grid[i][j]==2)
+                if(grid[i][j] == 2)
                 {
-                    visited[i][j] = 2; 
-                    q.push({{i,j}, 0}); 
+                    q.push({0, {i,j}});
                 }
-                else if(grid[i][j] == 1) fresh++; 
+                else if(grid[i][j] == 1) fresh++;
             }
         }
 
-        vector<int> frow = {-1, 0, 1, 0}; 
-        int fcol[4] = {0, 1, 0, -1}; 
-        int count = 0; 
+        int mxtime = 0;
+        vector<int>frow = {-1, 0, 1, 0};
+        vector<int>fcol = {0, 1, 0, -1};
+        int cnt = 0;
 
-        int max_time = 0; 
         while(!q.empty())
         {
-            int x = q.front().first.first; 
-            int y = q.front().first.second; 
-            int time = q.front().second; 
-            q.pop(); 
+            int x = q.front().second.first;
+            int y = q.front().second.second;
+            int time = q.front().first;
+            mxtime = max(mxtime, time);
+            q.pop();
 
-            max_time = max(time, max_time); 
-            for(int p=0; p<4; p++)
+            for(int i=0; i<4; i++)
             {
-                int nrow = x + frow[p];
-                int ncol = y + fcol[p]; 
+                int ax = x + frow[i];
+                int ay = y + fcol[i];
 
-                if(nrow>=0 && nrow<m && ncol>=0 && ncol<n
-                && !visited[nrow][ncol] && grid[nrow][ncol]==1)
+                if(ax>=0 && ax<m && ay>=0 && ay<n && grid[ax][ay] == 1)
                 {
-                    grid[nrow][ncol] = 2; 
-                    visited[nrow][ncol] = 1; 
-                    q.push({{nrow, ncol}, time + 1});
-                    count++;  
+                    cnt++;
+                    grid[ax][ay] = 2;
+                    q.push({time+1, {ax, ay}});
                 }
             }
         }
 
-        if (count == fresh) return max_time; 
-        return -1; 
+        if(cnt != fresh) return -1;
+        return mxtime;
+        
     }
 };
