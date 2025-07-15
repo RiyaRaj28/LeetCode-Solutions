66c9378632b31bbb739d77faaa9cf03feb723545
Riya Raj <rrajj7674@gmail.com>
2025-07-16 00:26:02 +0530

    Time: 3 ms (33%), Space: 12.9 MB (65.33%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.cpp
+++ 0684-redundant-connection/0684-redundant-connection.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
 
-    vector<int>parent; 
+    vector<int>parent;
     vector<int>rank; 
 
     int find(int x)
@@ -11,92 +11,46 @@ public:
         return parent[x] = find(parent[x]);
     }
 
-    bool Union(int x, int y)
+    bool Unite(int x, int y)
     {
         int xp = find(x); 
-        int yp = find(y); 
+        int yp = find(y);
 
-        if(xp == yp) return false; 
+        if(xp == yp) return false;
+
         if(rank[xp] > rank[yp])
         {
             parent[yp] = xp;
             rank[xp] += 1;
         }
-        else if(rank[xp] < rank[yp])
+        else if(rank[yp] > rank[xp])
         {
             parent[xp] = yp; 
-            rank[yp] += 1; 
+            rank[yp] += 1;
         }
         else
         {
-            parent[xp] = yp; 
-            rank[yp] += 1; 
+            parent[xp] = yp;
+            rank[xp] += 1;
         }
-
-        return true; 
+        return true;
     }
+
     vector<int> findRedundantConnection(vector<vector<int>>& edges) 
     {
         int n = edges.size();
-        // vector<vector<int>>adj(n+1);
-
-        // for(auto &vec : edges)
-        // {
-        //     int u = vec[0]; 
-        //     int v = vec[1]; 
-
-        //     adj[u].push_back(v); 
-        //     adj[v].push_back(u); 
-        // }
-
-        // queue<int>q; 
-        // vector<bool>visited(n+1, false); 
-        // set<pair<int, int>>st; 
-
-        // q.push(1); 
-        // while(!q.empty())
-        // {
-        //     int node = q.front(); 
-        //     q.pop();
-
-        //     if(visited[node]) continue; 
-        //     visited[node] = true; 
-
-        //     for(int &anode : adj[node])
-        //     {
-        //         if(!visited[anode]) q.push(anode); 
-        //         else st.insert({node, anode}); 
-        //     }
-
-        // }
-
-        // for(int i=n-1; i>=0; i--)
-        // {
-        //     int u = edges[i][0];
-        //     int v = edges[i][1]; 
-
-        //     if(st.find({u, v}) != st.end())
-        //     {
-        //         return {u,v};
-        //     }
-
-        // }
-
-        // return {-1, -1}; 
-
-        parent.resize(n+1); 
-        for(int i=0; i<n+1; i++) parent[i] = i; 
-
         rank.resize(n+1, 1);
-        for(auto &vec : edges)
+        parent.resize(n+1, 0);
+
+        for(int i=0; i<=n; i++) parent[i] = i; 
+
+        for(auto &edge : edges)
         {
-            int u = vec[0];
-            int v = vec[1]; 
+            int u = edge[0];
+            int v = edge[1]; 
 
-            if(!Union(u, v)) return vec;
+            if(!Unite(u, v)) return {u, v};
         }
-
         return {-1, -1}; 
-        
     }
 };
